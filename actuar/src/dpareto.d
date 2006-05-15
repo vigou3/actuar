3d36ccc3d7818cfa6835d0b67cd46b364e5e4b06
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-05-15 20:37:09 +0000

    Suppression de fichiers inutiles.

--- actuar/src/dpareto.d
+++
@@ -1,3 +0,0 @@
-dpareto.o: dpareto.c c:/DOCUME~1/mapig/R/R-23~1.0/include/R.h \
- c:/DOCUME~1/mapig/R/R-23~1.0/include/Rmath.h \
- c:/DOCUME~1/mapig/R/R-23~1.0/include/dpq.h
--- actuar/src/rpareto.d
+++
@@ -1,3 +0,0 @@
-rpareto.o: rpareto.c c:/DOCUME~1/mapig/R/R-23~1.0/include/R.h \
- c:/DOCUME~1/mapig/R/R-23~1.0/include/Rmath.h \
- c:/DOCUME~1/mapig/R/R-23~1.0/include/dpq.h
