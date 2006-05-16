eb7281e7017b57dd1413db7b1f4b384d41e5584e
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-05-16 14:41:34 +0000

    On s'approche d'une version qui marche.

--- actuar/src/zzz.c
+++ actuar/src/zzz.c
@@ -1,12 +1,13 @@
-#include <R.h>
 #include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+#include "actuar.h"
 
-const R_CMethodDef CEntries[]  = {
+static const R_CMethodDef CEntries[]  = {
     {"rpareto", &rpareto, 4, {INTSXP, REALSXP, REALSXP, REALSXP}},
     {NULL, NULL, 0}
 }
 
-R_init_actuar(DllInfo *dll)
+void R_init_actuar(DllInfo *dll)
 {
     R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
 }
