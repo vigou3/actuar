22e8a077ad22f82113f263a5ff11cb76a2a37f9e
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-05-16 17:12:00 +0000

    Ajout de commentaires et modification du nom des arguments.

--- actuar/R/pareto.R
+++ actuar/R/pareto.R
@@ -1,3 +1,12 @@
+### Definition of the {d,p,q,r}pareto functions. The Pareto
+### distribution used in these functions has cumulative distribution
+### function
+###
+###   Pr[X <= x] = 1 - (scale/(x + scale))^shape, x > 0.
+###
+### See Appendix A of Klugman, Panjer & Willmot, Loss Models, Second
+### Edition, Wiley, 2004.
+
 ## dpareto <- function (x, alpha, lambda)
 ##     .C("R_dpareto", as.double(x), as.double(alpha), as.double(lambda),
 ##        y = double(length(x)), as.integer(length(x)))$y
@@ -10,6 +19,6 @@
 ##     .C("R_qpareto", as.double(q), as.double(alpha), as.double(lambda),
 ##        y = double(length(x)), as.integer(length(x)))$y
 
-rpareto <- function(n, alpha, lambda)
-    .C("rpareto_sym", as.integer(n), as.double(alpha), as.double(lambda),
+rpareto <- function(n, shape, scale)
+    .C("rpareto_sym", as.integer(n), as.double(shape), as.double(scale),
        y = double(n))$y
