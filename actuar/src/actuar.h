bb7ab529c4cf02f8354f67d04a58df02b96f7f13
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-05-17 03:35:22 +0000

    Mise à jour du fichier d'en-têtes.

--- actuar/src/actuar.h
+++ actuar/src/actuar.h
@@ -1,5 +1,5 @@
 void dpareto(double *x, double *alpha, double *lambda, double *y, int *length);
 void ppareto(double *x, double *alpha, double *lambda, double *y, int *length);
 void qpareto(double *p, double *alpha, double *lambda, double *y, int *length);
-void rpareto(int *n, double *alpha, double *lambda, double *y);
+void rpareto(int *n, double *a, int *na, double *b, int *nb, double *x);
 
