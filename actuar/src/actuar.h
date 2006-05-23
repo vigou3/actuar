8c74b25a7b896bafff974f9cde7c982ad0f49c72
Mathieu Pigeon <pigeon.mathieu.2@uqam.ca>
2006-05-23 20:45:50 +0000

    En-têtes de localisation dans locale.h plutôt que actuar.h.

--- actuar/src/actuar.h
+++ actuar/src/actuar.h
@@ -29,11 +29,3 @@ typedef struct {
 } FUNTAB;
 extern FUNTAB fun_tab[];
 
-/* Translations */
-#include <R.h>
-#ifdef ENABLE_NLS
-#include <libintl.h>
-#define _(String) dgettext ("actuar", String)
-#else
-#define _(String) (String)
-#endif
