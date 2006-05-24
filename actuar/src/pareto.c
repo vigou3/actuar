530d53088a63a6a600303ff6508b35262f61b58d
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-05-24 19:06:33 +0000

    Correction de petites erreurs de syntaxe dans ppareto() et qpareto().

--- actuar/src/pareto.c
+++ actuar/src/pareto.c
@@ -20,7 +20,7 @@ double dpareto(double x, double shape, double scale, int give_log)
 	x < 0.0) 
 	error(_("invalid arguments"));
     
-    return  (give_log ?
+    return (give_log ?
 	log(shape) + shape * log(scale) - (shape + 1.0) * log(x + scale) :
 	shape * R_pow(scale, shape) / R_pow(x + scale, shape + 1.0));
 }
@@ -37,7 +37,7 @@ double ppareto(double x, double shape, double scale, int lower_tail, int log_p)
 	return (log_p ? 
 	  log(1.0 - exp(shape * (log(scale) - log(x + scale)))) : 
 	  1.0 - R_pow(scale / (x + scale), shape));
-    else:  
+    else  
         return (log_p ?
 	  shape * (log(scale) - log(x + scale)):
           R_pow(scale / (scale + x), shape));
@@ -55,7 +55,7 @@ double qpareto(double p, double shape, double scale, int lower_tail, int log_p)
     return (log_p ?
       log(scale) + log(1.0 - R_pow(p, 1.0 / shape)) - (1.0 / shape) * log(p):
       scale * (1 - R_pow(p, 1.0 / shape)) / R_pow(p, 1.0 / shape));
-  else: 
+  else
     return (log_p ?
       log(scale) + log(1.0 - R_pow(1.0 - p, 1.0 / shape)) - (1.0 / shape) * log(1.0 - p):
       scale * (1.0 - R_pow(1.0 - p, 1.0 / shape)) / R_pow(1.0 - p, 1.0 / shape));
