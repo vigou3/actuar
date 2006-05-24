f2c9d1bea3943a58f22a09e194a784797f90be27
Mathieu Pigeon <pigeon.mathieu.2@uqam.ca>
2006-05-24 17:40:30 +0000

    corrections dans (dpqr)pareto.c, dans burr.c et dans betatrans.c.

--- actuar/src/betatrans.c
+++ actuar/src/betatrans.c
@@ -24,11 +24,37 @@ double dbetatrans(double x, double shape, double scale, double gamma, double tau
 	x < 0.0) 
 	error(_("invalid arguments"));
     
-    return  give_log ?
+    return  (give_log ?
 	log(scale) + (1.0 / gamma) * (dbeta(x, tau, shape, 1) - log(1.0 - dbeta(x, tau, alpha, 0))) :
-	scale * R_pow(dbeta(x, tau, shape, 0)/(1.0 - dbeta(x, tau, shape, 0)), 1.0 / gamma);
+	scale * R_pow(dbeta(x, tau, shape, 0)/(1.0 - dbeta(x, tau, shape, 0)), 1.0 / gamma));
 }
 
+double pbetatrans(double q, double shape, double scale, double gamma, double tau, int lower_tail, int log_p)
+{
+    double u;
+
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	!R_FINITE(gamma) ||
+	!R_FINITE(tau) ||
+	shape <= 0.0 || 
+	scale <= 0.0 || 
+	gamma <= 0.0 || 
+	tau <= 0.0 || 
+	x < 0.0) 
+	error(_("invalid arguments"));
+
+    u = R_pow(x / scale, gamma) / (1.0 + R_pow(x / scale, gamma));
+    
+    if (lower_tail)
+	return (log_p ? 
+	  pbeta(u, tau, shape, 1, 1): 
+	  pbeta(u, tau, shape, 1, 0));
+    else:  
+        return (log_p ?
+	  pbeta(u, tau, shape, 0, 1):
+          pbeta(u, tau, shape, 0, 0));
+}
 
 double rbetatrans(double shape, double scale, double gamma, double tau)
 {
--- actuar/src/burr.c
+++ actuar/src/burr.c
@@ -22,9 +22,28 @@ double dburr(double x, double shape, double scale, double gamma, int give_log)
 	x < 0.0) 
 	error(_("invalid arguments"));
     
-    return  give_log ?
+    return  (give_log ?
 	log(shape) + log(gamma) + gamma * (log(x) - log(scale)) - log(x) - (shape + 1.0) * (log(R_pow(scale, gamma) + R_pow(x, gamma)) - gamma * log(scale)) :
-	shape * gamma * R_pow(x / scale, gamma) / (x * R_pow(1.0 + R_pow(x / scale, gamma), shape + 1.0));
+	shape * gamma * R_pow(x / scale, gamma) / (x * R_pow(1.0 + R_pow(x / scale, gamma), shape + 1.0)));
+}
+
+double pburr(double x, double shape, double scale, double gamma, int lower_tail, int log_p)
+{
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	!R_FINITE(gamma) ||
+	shape <= 0.0 || 
+	scale <= 0.0 || 
+	gamma <= 0.0 || 
+	x < 0.0) 
+	error(_("invalid arguments"));
+
+    if (lower_tail)
+	return log_p ? 
+	  
+    else:  
+        return log_p ?
+	  
 }
 
 double rburr(double shape, double scale, double gamma)
