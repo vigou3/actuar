be8f745407a0204540d1d3a342159da15013503b
Mathieu Pigeon <pigeon.mathieu.2@uqam.ca>
2006-05-24 19:41:55 +0000

    test

--- actuar/src/betatrans.c
+++ actuar/src/betatrans.c
@@ -12,7 +12,7 @@
 #include "locale.h"
 
 double dbetatrans(double x, double shape, double scale, double gamma, double tau, int give_log)
-{
+{ 
     if (!R_FINITE(shape) ||
 	!R_FINITE(scale) ||
 	!R_FINITE(gamma) ||
@@ -25,8 +25,8 @@ double dbetatrans(double x, double shape, double scale, double gamma, double tau
 	error(_("invalid arguments"));
     
     return  (give_log ?
-	log(scale) + (1.0 / gamma) * (dbeta(x, tau, shape, 1) - log(1.0 - dbeta(x, tau, alpha, 0))) :
-	scale * R_pow(dbeta(x, tau, shape, 0)/(1.0 - dbeta(x, tau, shape, 0)), 1.0 / gamma));
+	     dbeta(x, tau, shape, 1) + log(gamma) - log(scale) + (gamma - 1.0)*(log(x) - log(scale)) + 2.0 * (-log(1.0 + exp(gamma * (log(x) - log(scale))))) :
+	     dbeta(x, tau, shape, 0) * (gamma / scale) * R_pow(x / scale, gamma - 1.0) * R_pow(1.0 / (1.0 + R_pow(x / scale), gamma), 2.0));
 }
 
 double pbetatrans(double q, double shape, double scale, double gamma, double tau, int lower_tail, int log_p)
@@ -40,20 +40,35 @@ double pbetatrans(double q, double shape, double scale, double gamma, double tau
 	shape <= 0.0 || 
 	scale <= 0.0 || 
 	gamma <= 0.0 || 
-	tau <= 0.0 || 
-	x < 0.0) 
+	tau <= 0.0) 
 	error(_("invalid arguments"));
 
     u = R_pow(x / scale, gamma) / (1.0 + R_pow(x / scale, gamma));
+
+    if (x <= 0)
+	return R_DT_0;
     
-    if (lower_tail)
-	return (log_p ? 
-	  pbeta(u, tau, shape, 1, 1): 
-	  pbeta(u, tau, shape, 1, 0));
-    else:  
-        return (log_p ?
-	  pbeta(u, tau, shape, 0, 1):
-          pbeta(u, tau, shape, 0, 0));
+    return (lower_tail ? R_D_exp(pbeta(u, tau, shape, 1, 0)):
+	    R_D_exp(pbeta(u, tau, shape, 0, 0)));
+}
+
+double qbetatrans(double p, double shape, double scale, double gamma, double tau,  int lower_tail, int log_p)
+{
+
+  if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	!R_FINITE(gamma) ||
+	!R_FINITE(tau) ||
+	shape <= 0.0 || 
+	scale <= 0.0 || 
+	gamma <= 0.0 || 
+	tau <= 0.0) 
+	error(_("invalid arguments"));
+
+    R_Q_P01_boundaries(p, 0, 1);
+
+    return (lower_tail ? R_D_exp(log(scale) + gamma * (qbeta(p, tau, shape, 1, 1)) - gamma * log(1.0 - qbeta(p, tau, shape, 1, 0))) :
+	    R_D_exp(log(scale) + gamma * (1beta(p, tau, shape, 0, 1)) - gamma * log(1.0 - qbeta(p, tau, shape, 0, 0))));
 }
 
 double rbetatrans(double shape, double scale, double gamma, double tau)
