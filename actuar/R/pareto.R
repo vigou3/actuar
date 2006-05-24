cc31094c2e2690dfab535575a918fecedf3454b7
Mathieu Pigeon <pigeon.mathieu.2@uqam.ca>
2006-05-24 16:53:13 +0000

    Corrections de coquilles

--- actuar/R/pareto.R
+++ actuar/R/pareto.R
@@ -11,14 +11,14 @@
 ###
 ### AUTHORS:  Mathieu Pigeon, Vincent Goulet <vincent.goulet@act.ulaval.ca>
 
-dpareto <- function (x, shape, scale)
-	.External("do_dpq", "dpareto", x, shape, scale)
+dpareto <- function (x, shape, scale, log)
+	.External("do_dpq", "dpareto", x, shape, scale, log)
      
-ppareto <- <- function (x, shape, scale)
-	.External("do_dpq", "ppareto", x, shape, scale)
+ppareto <- function (x, shape, scale, tail, log)
+	.External("do_dpq", "ppareto", x, shape, scale, tail, log)
 
-qpareto <- function (q, shape, scale)
-	.External("do_dpq", "qpareto", q, shape, scale)
+qpareto <- function (q, shape, scale, tail, log)
+	.External("do_dpq", "qpareto", q, shape, scale, tail, log)
 
 rpareto <- function(n, shape, scale)
     .External("do_random", "rpareto", n, shape, scale)
