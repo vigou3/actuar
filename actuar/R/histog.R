3d057a0777cca29e2f5e5d6e57676a0b82384d34
Mathieu Pigeon <pigeon.mathieu.2@uqam.ca>
2006-06-12 15:05:22 +0000

    Amélioration des fonctions histog.R et ogive.R: Ajout d'options Traitement des bornes infinies Ajout d'une méthode pour superposer des ogives

--- actuar/R/histog.R
+++ actuar/R/histog.R
@@ -2,23 +2,28 @@
 ###
 ### Histogramme pour données groupées
 
-histog <- function(data, ...)
+histog <- function(data, xlim = NULL, ylim = NULL, xlab = "group boundaries", ylab = "f(x)")
 {
   class(data) <- "histog"
-  plot(data)
+  plot(data, xlim, ylim, xlab, ylab)
 }
 
 
-plot.histog <- function (data, ...)
+plot.histog <- function (data, xlim, ylim, xlab, ylab)
 {
   cj <- data$cj
   nj <- data$nj
 
-  fnt <- approxfun(cj, c(0, nj[-1]/(sum(nj) * diff(cj))),
-                 yleft=0, yright=0, f=1, method="constant")
+  fnt <- approxfun(cj, c(0, nj[-1] / (sum(nj) * diff(cj))),
+                 yleft = 0, yright = 0, f = 1, method = "constant")
 
-  plot(cj, fnt(cj), type="S", frame=FALSE) 
+  plot(cj, fnt(cj), main = "Histogram", xlim = xlim, ylim = ylim, xlab = xlab, ylab =ylab, type = "S", frame = FALSE) 
   segments(cj, 0, cj, fnt(cj))             
-  segments(0, 0, max(cj), 0)    
+  segments(0, 0, max(cj), 0)
+  
+  p<-sum(is.na(cj*0))
+  if(p >= 1){
+	warning("Group boundaries contain Inf")
+     } 
 }
   
--- actuar/R/ogive.R
+++ actuar/R/ogive.R
@@ -2,21 +2,44 @@
 ###
 ### Ogive pour données groupées
 
-ogive <- function(data, ...)
+ogive <- function(data, lines = FALSE, xlim = NULL, ylim = NULL, xlab = "group boundaries", ylab = "F(x)", col = 1)
 {
   class(data) <- "ogive"
-  plot(data)
+  
+  if (lines == FALSE){
+  plot(data, xlim, ylim, xlab, ylab, col)
+  }
+  else{
+  lines(data, xlim = xlim, ylim = ylim, col = col)
+  }
 }
 
 
-plot.ogive <- function (data, ...)
+plot.ogive <- function (data, xlim, ylim, xlab, ylab, col)
 {
   cj <- data$cj
   nj <- data$nj
 
-  Fnt <- approxfun(cj, cumsum(nj)/sum(nj),
-                 yleft=0, yright=1, method="linear")
+  Fnt <- approxfun(cj, cumsum(nj) / sum(nj),
+                 yleft = 0, yright = 1, method = "linear")
 
-  plot(cj, Fnt(cj), type="o", pch=20)
+  plot(cj, Fnt(cj), main = "Ogive", xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab, type = "o", pch = 20, col = col)
+  
+  p<-sum(is.na(cj*0))
+  if(p >= 1){
+	warning("Group boundaries contain Inf")
+      }
 }
+
+lines.ogive <- function (data, xlim, ylim, col)
+{
+  cj <- data$cj
+  nj <- data$nj
+  
+  Fnt <- approxfun(cj, cumsum(nj) / sum(nj),
+                 yleft = 0, yright = 1, method = "linear")
+
+  lines(cj, Fnt(cj), xlim = xlim, ylim = ylim, col = col, type = "o")
+}
+
   
