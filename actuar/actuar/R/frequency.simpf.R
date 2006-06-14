d3f960ee3eec2f5a4006060489c6022ecf2482a3
Louis-Philippe Pouliot <louis-philippe.pouliot.2@ulaval.ca>
2006-06-14 18:27:30 +0000

    Removed file/folder

--- actuar/actuar/R/frequency.simpf.R
+++
@@ -1,7 +0,0 @@
-frequency.simpf <- function(x, by = c("contract", "year"), ...)
-{
-    MARGIN <- match(by, c("contract", "year"))
-    apply(x$data, MARGIN, function(x) length(unlist(x)))
-}
-
-
--- actuar/actuar/R/mean.AggregateDist.R
+++
@@ -1,3 +0,0 @@
-mean.AggregateDist <- function(x) sum(x$fs * (1:length(x$fs)))
-
-var.AggregateDist <- function(x) sum(x$fs * (1:length(x$fs))^2) - (mean.AggregateDist(x))^2
