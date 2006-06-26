400735b3f66e425ef26b950579308186bd5467e3
Mathieu Pigeon <pigeon.mathieu.2@uqam.ca>
2006-06-26 18:18:04 +0000

    correction de la fonction pour faire une densité empirique groupée.

--- actuar/R/grouped.R
+++ actuar/R/grouped.R
@@ -42,11 +42,11 @@ hist.data.grouped <- function (x, y = NULL, freq = NULL, main = "Histogram", xli
      y <- x$nj
      x <- x$cj
    }
-  fnt <- approxfun(x, c(0, y[-1] / (sum(y) * diff(x))), yleft = 0, yright = 0, f = 1, method = "constant")
+  fnt <- approxfun(x, y / (sum(y) * diff(c(0, x))), yleft = 0, yright = 0, f = 1, method = "constant")
   r <- structure(list(cj = x, nj = y, density = fnt(x)), class = "histogram")
   if(plot){
-    plot(x, fnt(x), main = main, xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab, type = "S", frame = FALSE)
-    segments(x, 0, x, fnt(x))             
+    plot(c(0, x) , fnt(c(0, x)), main = main, xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab, type = "S", frame = FALSE)
+    segments(x, 0, x, fnt(x))
     segments(0, 0, max(x), 0)
     invisible(r)
   }
@@ -63,17 +63,22 @@ plot.groupedData <- function (x, y = NULL, xlim = NULL, ylim = NULL, xlab = "gro
 
 print.groupedData <- function (x, digits= getOption("digits") - 2, ...)
 {
-    numform <- function(x) paste(formatC(x, dig=digits), collapse=", ")
-    cat("Empirical CDF for grouped data \nCall: ")
-    print(attr(x, "call"), ...)
-    nc <- length(xxc <- eval(expression(x),env = environment(x)))
-    nn <- length(xxn <- eval(expression(y),env = environment(x)))
-    i1 <- 1:min(3, nc)
-    i2 <- if(nc >= 4) max(4, nc - 1):nc else integer(0)
-    i3 <- 1:min(3, nn)
-    i4 <- if(nn >= 4) max(4, nn - 1):nn else integer(0)
-    cat(" cj = ", numform(xxc[i1]), if(nc > 3) ", ", if(nc > 5) " ..., ", numform(xxc[i2]), "\n", sep = "")
-    cat(" nj = ", numform(xxn[i1]), if(nn > 3) ", ", if(nn > 5) " ..., ", numform(xxn[i2]), "\n", sep = "")
-    invisible(x)
+  numform <- function(x) paste(formatC(x, dig=digits), collapse=", ")
+  cat("Empirical CDF for grouped data \nCall: ")
+  print(attr(x, "call"), ...)
+  nc <- length(xxc <- eval(expression(x),env = environment(x)))
+  nn <- length(xxn <- eval(expression(y),env = environment(x)))
+  i1 <- 1:min(3, nc)
+  i2 <- if(nc >= 4) max(4, nc - 1):nc else integer(0)
+  i3 <- 1:min(3, nn)
+  i4 <- if(nn >= 4) max(4, nn - 1):nn else integer(0)
+  cat(" cj = ", numform(xxc[i1]), if(nc > 3) ", ", if(nc > 5) " ..., ", numform(xxc[i2]), "\n", sep = "")
+  cat(" nj = ", numform(xxn[i1]), if(nn > 3) ", ", if(nn > 5) " ..., ", numform(xxn[i2]), "\n", sep = "")
+  invisible(x)
 }
 
+print.data.grouped <- function(x, ...)
+{
+  
+
+}
