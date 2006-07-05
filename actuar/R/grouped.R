8e2d6a9ad845c6249aa866384463b07d26ad4ace
Mathieu Pigeon <pigeon.mathieu.2@uqam.ca>
2006-07-05 14:50:00 +0000

    Finalisation de la fonction "[".

--- actuar/R/grouped.R
+++ actuar/R/grouped.R
@@ -6,52 +6,71 @@
 ### See Klugman, Panjer & Willmot, Loss Models, Second
 ### Edition, Wiley, 2004.
 ###
-### AUTHORS: Vincent Goulet <vincent.gouletaact.ulaval.ca> Mathieu Pigeon
+### AUTHORS: Vincent Goulet <vincent.gouletaact.ulaval.ca>, Mathieu Pigeon
  
-
+ 
 grouped <- function(x, y = NULL, digits = 2)
 {
+  ## 'data.frame' must contain boundaries in first column and number of
+  ## data by class in second column.
   if(class(x) == "data.frame"){
     y <- x[, 2]
     x <- x[, 1]
   }
   nx <- length(x)
   ny <- length(y)
-  if(nx - ny > 1 || nx - ny < 0)
+
+  ## First data in 'y' won't be used. 
+  if(nx - ny > 1 || nx - ny < 0){
     stop("length(x) incorrect")
-  if(nx - ny == 1)
+  }
+  if(nx - ny == 1){
       y = c(0, y)
-  if(nx == ny && y[1] != 0)
+  }
+  if(nx == ny && y[1] != 0){
     stop("length(y) incorrect")
-   
-  res = list(cj = x, nj = y, digits = digits)
+  }
+
+  ## Create an object of class 'grouped.data'.
+  res = list(cj = x, nj = y, digits = digits, j = 3)
   class(res) <- c("grouped.data")
   attr(res, "call") <- sys.call()
   res
   }
 
+## Calculate an empirical distribution function.
 ogive <- function(x, y = NULL)
 {
-    if(class(x) == "grouped.data"){
-      y <- x$nj
-      x <- x$cj
-      }
-    if (length(x) < 1) 
-        stop("'x' must have 1 or more non-missing values")
-    Fnt <- approxfun(x, cumsum(y) / sum(y), yleft = 0, yright = 1, method = "linear", ties = "ordered")
-    class(Fnt) <- c("ogive", class(Fnt))
-    attr(Fnt, "call") <- sys.call()
-    Fnt
+  ## Use object created by 'grouped' function.
+  if(class(x) == "grouped.data"){
+    y <- x$nj
+    x <- x$cj
+  }
+  ## An error message is issued if 'x' is empty.
+  if (length(x) < 1) 
+    stop("'x' must have 1 or more non-missing values")
+
+  ##Create an object of class 'ogive'.
+  Fnt <- approxfun(x, cumsum(y) / sum(y), yleft = 0, yright = 1, method = "linear", ties = "ordered")
+  class(Fnt) <- c("ogive", class(Fnt))
+  attr(Fnt, "call") <- sys.call()
+  Fnt
 }
 
+## Calculate an empirical density function and create the histogram.
 hist.grouped.data <- function (x, y = NULL, main = "Histogram", xlim = NULL, ylim = NULL, xlab = "boundaries", ylab = "f(x)", plot = TRUE, ...)
 {
+  ## Use object created by 'grouped' function.
   if(class(x) == "grouped.data"){
-     y <- x$nj
-     x <- x$cj
-   }
+    y <- x$nj
+    x <- x$cj
+  }
+
+  ## Create an object of class 'histogram'.
   fnt <- approxfun(x, c(0, y[-1] / (sum(y) * diff(x))), yleft = 0, yright = 0, f = 1, method = "constant")
   r <- structure(list(cj = x, nj = y[-1], density = fnt(x)), class = "histogram")
+
+  ## If 'plot' is true, histogram is created, else, boudaries, number of data by class and density are returned.
   if(plot){
     plot(x , fnt(x), main = main, xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab, type = "S", frame = FALSE)
     segments(x, 0, x, fnt(x))
@@ -63,6 +82,7 @@ hist.grouped.data <- function (x, y = NULL, main = "Histogram", xlim = NULL, yli
   }
 }
 
+## Method to create graphic of empirical distribution function.
 plot.ogive <- function (x, y = NULL, xlim = NULL, ylim = NULL, xlab = "boundaries", ylab = "F(x)", col = 1, ...)
 {
   xval <- eval(expression(x), env = environment(x))
@@ -71,7 +91,10 @@ plot.ogive <- function (x, y = NULL, xlim = NULL, ylim = NULL, xlab = "boundarie
 
 print.ogive <- function (x, digits = getOption("digits") - 2, ...)
 {
+  ## To formate numbers.
   numform <- function(x) paste(formatC(x, dig = digits), collapse = ", ")
+
+  ## Create a structure for the presentation of empirical distribution function.
   cat("Empirical CDF for grouped data \nCall: ")
   print(attr(x, "call"), ...)
   nc <- length(xxc <- eval(expression(x), env = environment(x)))
@@ -87,30 +110,44 @@ print.ogive <- function (x, digits = getOption("digits") - 2, ...)
 
 print.grouped.data <- function(x, ...)
 {
-  digits <- x$digits
+  ## To formate numbers.
   numform <- function(x) formatC(x, digits = digits, format = "e")
   numformy <- function(x) formatC(x)
+
+  ## Use object created by 'grouped' function
   y <- x$nj
+  j <- x$j
+  digits <- x$digits
   x <- x$cj
   x1 <- length(x)
- 
+
+  ## Choose which column(s) is(are) presented.
+  if(j == 1)
+    cat("          cj     ", "\n", paste("[",numform(x[-x1]),", ",numform(x[-1]),")", "\n", sep = ""))
+  if(j == 2)
+    cat(" nj ", "\n", paste(numformy(y[-1]), "\n", sep = ""))
+  if(j != 1 && j != 2)
     cat("          cj     ", "          nj       ", "\n",paste("[",numform(x[-x1]),", ",numform(x[-1]),")", "       ", numformy(y[-1]), "\n", sep = ""))
 }
 
-"[.grouped.data" <- function(x, is = 1, ie = NULL)
+"[.grouped.data" <- function(x, i, j)
 {
-  if(is.null(ie)){
-    ie <- length(x$cj) - 1
+  ## If 'i' is missing, all rows are presented.
+  if(missing(i)){
+    i <- (1:(length(x$cj)-1))
   }
-  is <- is + 1
-  ie <- ie + 1
-  nj <- x$nj
-  nj <- nj[(is:ie)]
+  ## If 'j' is missing, all columns are presented.
+  if(missing(j)){
+    j <- 3
+  }
+
+  ## Create an object of class 'grouped.data'.
+  nj <- x$nj[-1]
+  nj <- nj[(i)]
   cj <- x$cj
-  is <- is - 1
-  cj <- cj[(is:ie)]
+  cj <- cj[c((i), max(i) + 1)]
   digits <- x$digits
-  res = list(cj = cj, nj =  c(0, nj), digits = digits)
+  res = list(cj = cj, nj =  c(0, nj), digits = digits, j = j)
   class(res) <- c("grouped.data")
   attr(res, "call") <- sys.call()
   res   
