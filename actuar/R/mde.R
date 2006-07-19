938713b1119f090ca65e2873440b9490d45bcb0d
Mathieu Pigeon <pigeon.mathieu.2@uqam.ca>
2006-07-19 17:57:11 +0000

    Nouvelle version de la fonction mde.R: elle n'est pas parfaite, mais elle ne fait plus n'importe quoi !

--- actuar/R/mde.R
+++ actuar/R/mde.R
@@ -11,30 +11,30 @@
 ###
 ### AUTHORS:  Mathieu Pigeon, Vincent Goulet <vincent.goulet@act.ulaval.ca>
 
-mde <- function(x, cdf, start, ...)
-  {
-    myfn <- function(parm, ...) drop(crossprod(dens(parm, k, ...) - e(k))) 
-    Call <- match.call(expand.dots = TRUE)
+mde <-function (x, cdf, start, ...) 
+{
+    myfn <- function(parm, ...) drop(crossprod(dens(parm, ...) - e(k)))
+    Call <- match.call(expand.dots = FALSE)
     if (missing(start)) 
-      start <- NULL
+        start <- NULL
     if (missing(x) || length(x) == 0 || mode(x) != "numeric") 
-      stop("'x' must be a non-empty numeric vector")
+        stop("'x' must be a non-empty numeric vector")
     if (missing(cdf) || !(is.function(cdf) || is.character(cdf))) 
-      stop("'cdf' must be supplied as a function or name")
+        stop("'cdf' must be supplied as a function or name")
     n <- length(x)
     if (is.null(start) || !is.list(start)) 
-      stop("'start' must be a named list")
+        stop("'start' must be a named list")
     nm <- names(start)
     f <- formals(cdf)
     args <- names(f)
     m <- match(nm, args)
     if (any(is.na(m))) 
-      stop("'start' specifies names which are not arguments to 'cdf'")
+        stop("'start' specifies names which are not arguments to 'cdf'")
     formals(cdf) <- c(f[c(1, m)], f[-c(1, m)])
     dens <- function(parm, x, ...) cdf(x, parm, ...)
     if ((l <- length(nm)) > 1) 
-      body(dens) <- parse(text = paste("cdf(x,", paste("parm[", 
-                            1:l, "]", collapse = ", "), ", ...)"))
+        body(dens) <- parse(text = paste("cdf(x,", paste("parm[", 
+            1:l, "]", collapse = ", "), ", ...)"))
     e <- ecdf(x)
     k <- knots(e)
     Call[[1]] <- as.name("optim")
@@ -44,16 +44,16 @@ mde <- function(x, cdf, start, ...)
     Call$fn <- myfn
     Call$hessian <- TRUE
     if (is.null(Call$method)) {
-      if (any(c("lower", "upper") %in% names(Call))) 
-        Call$method <- "L-BFGS-B"
-      else if (length(start) > 1) 
-        Call$method <- "BFGS"
-      else Call$method <- "Nelder-Mead"
+        if (any(c("lower", "upper") %in% names(Call))) 
+            Call$method <- "L-BFGS-B"
+        else if (length(start) > 1) 
+            Call$method <- "BFGS"
+        else Call$method <- "Nelder-Mead"
     }
     res <- eval(Call)
     if (res$convergence > 0) 
-      stop("optimization failed")
+        stop("optimization failed")
     res
-  }
+}
 
 
