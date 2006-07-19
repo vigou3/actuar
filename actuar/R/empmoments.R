c1b7fafe948936cff858577cb4b6f9237c4b2f75
Mathieu Pigeon <pigeon.mathieu.2@uqam.ca>
2006-07-19 14:52:45 +0000

    Ajout de fonctions (mde n'est pas valide, mais n'empêche pas la compilation)

---
+++ actuar/R/empmoments.R
@@ -0,0 +1,33 @@
+### ===== actuar: an R package for Actuarial Science =====
+###
+### Empirical moments for individual and grouped data.
+###
+### See Appendix A of Klugman, Panjer & Willmot, Loss Models, Second
+### Edition, Wiley, 2004.
+###
+### AUTHORS:  Mathieu Pigeon, Vincent Goulet <vincent.goulet@act.ulaval.ca>
+
+emp.moments <- function(...)
+  UseMethod("emp.moments")
+
+emp.moments.default <- function(x, order = 1, ...)
+  {
+    if(any(order < 0))
+      stop("order must be positive")
+    
+    sapply(order, function(n) mean(x^n))
+  }
+
+emp.moments.grouped.data <- function(x, order = 1, ...)
+  {
+    if(any(order < 0))
+      stop("order must be positive")
+    
+    mom.1 <- function(k, x)
+      {
+        n <- sum(x$nj)
+        sum(diff(x$cj^(k+1)) / diff(x$cj) / (k+1) * x$nj[-1] / n)
+      }
+    sapply(order, mom.1, x = x)
+  }
+
---
+++ actuar/R/mde.R
@@ -0,0 +1,59 @@
+### ===== actuar: an R package for Actuarial Science =====
+###
+### Definition of the {m,lev}exp functions. The Exponential
+### distribution used in these functions has cumulative distribution
+### function
+###
+###   Pr[X <= x] = 1 - exp(-x / scale), x > 0.
+###
+### See Appendix A of Klugman, Panjer & Willmot, Loss Models, Second
+### Edition, Wiley, 2004.
+###
+### AUTHORS:  Mathieu Pigeon, Vincent Goulet <vincent.goulet@act.ulaval.ca>
+
+mde <- function(x, cdf, start, ...)
+  {
+    myfn <- function(parm, ...) drop(crossprod(dens(parm, k, ...) - e(k))) 
+    Call <- match.call(expand.dots = TRUE)
+    if (missing(start)) 
+      start <- NULL
+    if (missing(x) || length(x) == 0 || mode(x) != "numeric") 
+      stop("'x' must be a non-empty numeric vector")
+    if (missing(cdf) || !(is.function(cdf) || is.character(cdf))) 
+      stop("'cdf' must be supplied as a function or name")
+    n <- length(x)
+    if (is.null(start) || !is.list(start)) 
+      stop("'start' must be a named list")
+    nm <- names(start)
+    f <- formals(cdf)
+    args <- names(f)
+    m <- match(nm, args)
+    if (any(is.na(m))) 
+      stop("'start' specifies names which are not arguments to 'cdf'")
+    formals(cdf) <- c(f[c(1, m)], f[-c(1, m)])
+    dens <- function(parm, x, ...) cdf(x, parm, ...)
+    if ((l <- length(nm)) > 1) 
+      body(dens) <- parse(text = paste("cdf(x,", paste("parm[", 
+                            1:l, "]", collapse = ", "), ", ...)"))
+    e <- ecdf(x)
+    k <- knots(e)
+    Call[[1]] <- as.name("optim")
+    Call$cdf <- Call$start <- NULL
+    Call$x <- k
+    Call$par <- start
+    Call$fn <- myfn
+    Call$hessian <- TRUE
+    if (is.null(Call$method)) {
+      if (any(c("lower", "upper") %in% names(Call))) 
+        Call$method <- "L-BFGS-B"
+      else if (length(start) > 1) 
+        Call$method <- "BFGS"
+      else Call$method <- "Nelder-Mead"
+    }
+    res <- eval(Call)
+    if (res$convergence > 0) 
+      stop("optimization failed")
+    res
+  }
+
+
