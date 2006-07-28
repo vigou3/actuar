9c9d10f6411380aeaade6d2c6d9483201eb8af5c
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-07-28 05:24:20 +0000

    Fonction elev() vectorielle. Méthodes ajoutées pour knots() et print(). Reste à faire la méthode pour plot().

--- actuar/R/elev.R
+++ actuar/R/elev.R
@@ -6,52 +6,81 @@
 ### AUTHOR: Vincent Goulet <vincent.goulet@act.ulaval.ca>
 
 elev <- function(x, ...)
+{
+    Call <- match.call()
     UseMethod("elev")
+}
 
 elev.default <- function(x)
 {
-    call <- match.call()
+    if (!exists("Call", inherits = FALSE))
+        Call <- match.call()
     FUN <- function(limit)
         colMeans(sapply(limit, pmin, x = x))
-    assign("x", x, env = environment(FUN))
-    assign("call", call, env = environment(FUN))
+    assign("x", sort(x), env = environment(FUN))
     class(FUN) <- c("elev", class(FUN))
+    attr(FUN, "call") <- Call
+    attr(FUN, "grouped") <- FALSE
     FUN
 }
 
 elev.grouped.data <- function(x)
 {
-    call <- match.call()
+    if (!exists("Call", inherits = FALSE))
+        Call <- match.call()
     FUN <- function(limit)
     {
-        ## class in which the limit is located
-        cl <- cut(limit, cj, right = TRUE, labels = FALSE)
+        ## Number of classes.
+        r <- length(nj)
 
-        ## means for all classes below the limit
-        cjt <- head(cj, cl)             # up to the upper limit
-        # valid in R 2.4.0 only
-        # res1 <- drop(crossprod(((head(cjt, -1) + tail(cjt, -1))/2,
-        #                         head(nj, cl - 1))
-        res1 <- drop(crossprod((cjt[-1] + cjt[-length(cjt)])/2,
-                               head(nj, cl - 1)))
+        ## Class in which the limit is located.
+        cl <- cut(limit, cj, include.lowest = TRUE, labels = FALSE)
 
-        ## mean for class with the limit
-        cjt <- cj[cl]                   # lower limit of the class
-        njt <- nj[cl]                   # frequency in the class
-        p <- (limit - cjt) / (cj[cl + 1] - cjt) # prop. on each side
-        res2 <- njt * p * mean(c(cjt, limit)) + njt * (1 - p) * limit
+        ## Means for all classes below each limit.
+        cjt <- head(cj, max(cl))        # upper bounds
+        res1 <- sapply(cl - 1, function(n, x)
+                       drop(crossprod(head(x, n), head(nj, n))),
+                       (cjt[-1] + cjt[-length(cjt)])/2)
 
-        ## means for all classes above the limit
-        # valid with r 2.4.0 only
-        # res3 <- sum(limit * tail(nj, -cl))
-        res3 <- sum(limit * nj[-seq(to = cl)])
+        ## Means for classes with each limit.
+        cjt <- cj[cl]                   # lower bounds
+        njt <- nj[cl]                   # frequencies
+        p <- (limit - cjt) / (cj[cl + 1] - cjt) # prop. to take
+        res2 <- njt * p * (cjt + limit)/2 + njt * (1 - p) * limit
+
+        ## Means for classes above each limit.
+        res3 <- limit * sapply(r - cl, function(n, x) sum(tail(x, n)),
+                               nj[-(1:min(cl))])
 
         ## Total
         (res1 + res2 + res3)/sum(nj)
     }
     assign("cj", x$cj, env = environment(FUN))
     assign("nj", x$nj[-1], env = environment(FUN))
-    assign("call", call, env = environment(FUN))
     class(FUN) <- c("elev", class(FUN))
+    attr(FUN, "call") <- Call
+    attr(FUN, "grouped") <- TRUE
     FUN
 }
+
+knots.elev <- function(fn, ...)
+    eval(expression(cj), env = environment(fn))
+
+print.elev <- function(x, digits = getOption("digits") - 2, ...)
+{
+    numform <- function(x) paste(formatC(x, dig = digits), collapse = ", ")
+
+    varname <- if (attr(x, "grouped")) "cj" else "x"
+    cat("Empirical LEV \nCall: ")
+    print(attr(x, "call"), ...)
+    n <- length(xx <- eval(parse(text = varname), env = environment(x)))
+    i1 <- 1:min(3, n)
+    i2 <- if (n >= 4) max(4, n - 1):n else integer(0)
+    cat(" ", varname, "[1:", n, "] = ", numform(xx[i1]), if (n > 3)
+        ", ", if (n > 5)
+        " ..., ", numform(xx[i2]), "\n", sep = "")
+    invisible(x)
+}
+
+### !!! To do !!!
+# plot.elev <- function()
