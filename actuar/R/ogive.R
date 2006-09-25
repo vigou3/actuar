5bc9c872d4e730a2f0207f73ca6a2456d395a8a6
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-09-25 05:25:09 +0000

    Ajustements à ogive() consécutifs aux changements dans grouped.data(). Correction d'un petit bogue dans la méthode de print().

--- actuar/R/ogive.R
+++ actuar/R/ogive.R
@@ -1,49 +1,54 @@
-
 ### ===== actuar: an R package for Actuarial Science =====
 ###
-### Ogive and histogram for grouped data
+### Ogive for grouped data
 ###
 ### See Klugman, Panjer & Willmot, Loss Models, Second
 ### Edition, Wiley, 2004.
 ###
 ### AUTHORS: Vincent Goulet <vincent.goulet@act.ulaval.ca>, Mathieu Pigeon
 
-## Calculate an empirical distribution function.
 ogive <- function(x, y = NULL, ...)
 {
-    ## Use object created by 'grouped' function.
-    if (class(x) == "grouped.data")
+    ## Can compute the ogive either from an object of class
+    ## 'grouped.data', or from a vector of class boundaries and a
+    ## vector of class frequencies. The second vector is one element
+    ## shorter than the first.
+    if (inherits(x, "grouped.data"))
     {
-        y <- x$nj
-        x <- x$cj
+        y <- x[, 2]
+        x <- get("cj", environment(x))
     }
-    ## An error message is issued if 'x' is empty.
-    if (length(x) < 1)
-      stop("'x' must have 1 or more non-missing values")
+    else
+    {
+        if (length(x) - length(y) != 1)
+            stop("incorrect number of class boundaries and frequencies")
+    }
 
-    ##Create an object of class 'ogive'.
-    Fnt <- approxfun(x, cumsum(y) / sum(y), yleft = 0, yright = 1, method = "linear", ties = "ordered")
-    class(Fnt) <- c("ogive", class(Fnt))
-    attr(Fnt, "call") <- sys.call()
-    Fnt
+    ## Create an object of class 'ogive'.
+    res <- approxfun(x, cumsum(c(0, y)) / sum(y), yleft = 0, yright = 1,
+                     method = "linear", ties = "ordered")
+    class(res) <- c("ogive", class(res))
+    attr(res, "call") <- sys.call()
+    res
 }
 
 print.ogive <- function(x, digits = getOption("digits") - 2, ...)
 {
-    ## To formate numbers.
+    ## Utility function
     numform <- function(x) paste(formatC(x, dig = digits), collapse = ", ")
 
-    ## Create a structure for the presentation of empirical distribution function.
+    ## The rest is adapted from ecdf()
     cat("Empirical CDF for grouped data \nCall: ")
     print(attr(x, "call"), ...)
-    nc <- length(xxc <- eval(expression(x), env = environment(x)))
-    nn <- length(xxn <- eval(expression(y), env = environment(x)))
+    nc <- length(xxc <- get("x", env = environment(x)))
+    nn <- length(xxn <- get("y", env = environment(x)))
     i1 <- 1:min(3, nc)
     i2 <- if (nc >= 4) max(4, nc - 1):nc else integer(0)
     i3 <- 1:min(3, nn)
     i4 <- if (nn >= 4) max(4, nn - 1):nn else integer(0)
-    cat(" cj = ", numform(xxc[i1]), if (nc > 3) ", ", if (nc > 5) " ..., ", numform(xxc[i2]), "\n", sep = "")
-    cat(" nj = ", numform(xxn[i1]), if (nn > 3) ", ", if (nn > 5) " ..., ", numform(xxn[i2]), "\n", sep = "")
+    cat("    x = ", numform(xxc[i1]), if (nc > 3) ", ",
+        if (nc > 5) " ..., ", numform(xxc[i2]), "\n", sep = "")
+    cat(" F(x) = ", numform(xxn[i3]), if (nn > 3) ", ",
+        if (nn > 5) " ..., ", numform(xxn[i4]), "\n", sep = "")
     invisible(x)
 }
-
