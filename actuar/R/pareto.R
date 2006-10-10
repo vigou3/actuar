ce379a57f4e8792e87a935ccffdfbcc06c605281
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-10 19:27:38 +0000

    Révision de la Pareto complétée.

--- actuar/R/pareto.R
+++ actuar/R/pareto.R
@@ -1,8 +1,8 @@
 ### ===== actuar: an R package for Actuarial Science =====
 ###
-### Definition of the {d,p,q,r,m,lev}pareto functions. The Pareto
-### distribution used in these functions has cumulative distribution
-### function
+### Definition of the {d,p,q,r,m,lev}pareto functions to compute
+### characteristics of the Pareto distribution. The version used in
+### these functions has cumulative distribution function
 ###
 ###   Pr[X <= x] = 1 - (scale/(x + scale))^shape, x > 0.
 ###
@@ -23,11 +23,11 @@ qpareto <- function (p, shape, scale, lower.tail = TRUE, log.p = FALSE)
 rpareto <- function(n, shape, scale)
     .External("do_random", "rpareto", n, shape, scale)
 
-mpareto <- function(k, shape, scale, log = FALSE)
-     .External("do_dpq", "mpareto", k, shape, scale, log)
+mpareto <- function(order, shape, scale, log = FALSE)
+     .External("do_dpq", "mpareto", order, shape, scale, log)
 
-levpareto <- function(d, shape, scale, order = 1, log = FALSE)
-     .External("do_dpq", "levpareto", d, shape, scale, order, log)
+levpareto <- function(limit, shape, scale, order = 1, log = FALSE)
+     .External("do_dpq", "levpareto", limit, shape, scale, order, log)
 
 ## Aliases
 dpareto2 <- dpareto
--- actuar/src/pareto.c
+++ actuar/src/pareto.c
@@ -1,8 +1,8 @@
 /*  ===== actuar: an R package for Actuarial Science =====
  *
- *  Fonctions to compute density, cumulative distribution and quantile
- *  fonctions of the Pareto distribution, to calculate raw moments and limited moments 
- *  of the random variable and to simulate random variates. See ../R/pareto.R for details.
+ *  Functions to compute density, cumulative distribution and quantile
+ *  functions, raw and limited moments and to simulate random variates
+ *  for the Pareto distribution. See ../R/pareto.R for details.
  *
  *  AUTHORS: Mathieu Pigeon and Vincent Goulet <vincent.goulet@act.ulaval.ca>
  */
@@ -14,95 +14,94 @@
 
 double dpareto(double x, double shape, double scale, int give_log)
 {
-    if (!R_FINITE(shape) || 
+    if (!R_FINITE(shape) ||
 	!R_FINITE(scale) ||
-	shape <= 0.0 || 
-	scale <= 0.0) 
-      return R_NaN;
+	shape <= 0.0 ||
+	scale <= 0.0)
+	return R_NaN;
 
-    if (!R_FINITE(x) || x < 0.0) 
-      return R_D_d0;
-    
-    return (give_log ?
+    if (!R_FINITE(x) || x < 0.0)
+	return R_D_d0;
+
+    return give_log ?
 	log(shape) + shape * log(scale) - (shape + 1.0) * log(x + scale) :
-	shape * R_pow(scale, shape) / R_pow(x + scale, shape + 1.0));
+	shape * R_pow(scale, shape) / R_pow(x + scale, shape + 1.0);
 }
 
 double ppareto(double x, double shape, double scale, int lower_tail, int log_p)
 {
-  double tmp;
-  
-  if (!R_FINITE(shape) || 
-      !R_FINITE(scale) ||
-      shape <= 0.0 || 
-      scale <= 0.0)
-    return R_NaN;
-  
-  if (x <= 0)
-    return R_DT_0;
-  
-  tmp = log(scale) - log(x + scale);
-  
-  return (lower_tail ? R_D_exp(log(1.0 - exp(shape * (tmp)))):
-	  R_D_exp(shape * (tmp)));
+    double tmp;
+
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	shape <= 0.0 ||
+	scale <= 0.0)
+	return R_NaN;
+
+    if (x <= 0)
+	return R_DT_0;
+
+    tmp = log(scale) - log(x + scale);
+
+    return lower_tail ? R_D_exp(log(1.0 - exp(shape * (tmp)))) :
+	R_D_exp(shape * (tmp));
 }
 
 double qpareto(double p, double shape, double scale, int lower_tail, int log_p)
 {
-  double tmp, tmp1;
+    double tmp, tmp1;
 
-  if (!R_FINITE(shape) || 
+    if (!R_FINITE(shape) ||
 	!R_FINITE(scale) ||
-	shape <= 0.0 || 
+	shape <= 0.0 ||
 	scale <= 0.0)
 	return R_NaN;
 
-  R_Q_P01_boundaries(p, 0, R_PosInf);
-  tmp = R_D_qIv(p);
-  tmp1 = 1.0 / shape;
+    R_Q_P01_boundaries(p, 0, R_PosInf);
+    tmp = R_D_qIv(p);
+    tmp1 = 1.0 / shape;
 
-    return (lower_tail ? scale * (R_pow(1.0 - tmp, -tmp1) - 1.0) :
-	    scale * (R_pow(tmp, -tmp1) - 1.0));
+    return lower_tail ? scale * (R_pow(1.0 - tmp, -tmp1) - 1.0) :
+	scale * (R_pow(tmp, -tmp1) - 1.0);
 }
 
 double rpareto(double shape, double scale)
 {
-    if (!R_FINITE(shape) || 
+    if (!R_FINITE(shape) ||
 	!R_FINITE(scale) ||
-	shape <= 0.0 || 
+	shape <= 0.0 ||
 	scale <= 0.0)
 	return R_NaN;
 
     return scale * (R_pow(unif_rand(), -1.0 / shape) - 1.0);
 }
 
-double mpareto(double k, double shape, double scale, int give_log)
+double mpareto(double order, double shape, double scale, int give_log)
 {
-    if (!R_FINITE(shape) || 
+    if (!R_FINITE(shape) ||
 	!R_FINITE(scale) ||
-	!R_FINITE(k) ||
-	shape <= 0.0 || 
+	!R_FINITE(order) ||
+	shape <= 0.0 ||
 	scale <= 0.0 ||
-	k <= -1.0 ||
-	k >= shape)
+	order <= -1.0 ||
+	order >= shape)
 	return R_NaN;
 
-    return R_pow(scale, k) * gammafn(k + 1.0) * gammafn(shape - k) / gammafn(shape);
-
+    return R_pow(scale, order) * gammafn(order + 1.0) * gammafn(shape - order) / gammafn(shape);
 }
 
-double levpareto(double d, double shape, double scale, double order, int give_log)
+double levpareto(double limit, double shape, double scale, double order, int give_log)
 {
-  if (!R_FINITE(shape) || 
-      !R_FINITE(scale) ||
-      !R_FINITE(d) ||
-      !R_FINITE(order) ||
-      shape <= 0.0 || 
-      scale <= 0.0 ||
-      d <= 0.0 ||
-      order <= -1.0 ||
-      order >= shape)
-    return R_NaN;
-  
-    return R_pow(scale, order) * gammafn(order + 1.0) * gammafn(shape - order) * pbeta(d / (d + scale), order + 1.0, shape - order, 1, 0) / gammafn(shape) + R_pow(d, order) * R_pow(scale / (scale + d), shape);
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	!R_FINITE(limit) ||
+	!R_FINITE(order) ||
+	shape <= 0.0 ||
+	scale <= 0.0 ||
+	limit <= 0.0 ||
+	order <= -1.0 ||
+	order >= shape)
+	return R_NaN;
+
+    return R_pow(scale, order) * gammafn(order + 1.0) * gammafn(shape - order) * pbeta(limit / (limit + scale), order + 1.0, shape - order, 1, 0) / gammafn(shape) + R_pow(limit, order) * R_pow(scale / (scale + limit), shape);
 }
