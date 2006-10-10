c2449ef74b377c19b8a18ad9f45a724c1becaf5a
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-10 16:43:43 +0000

    Révision de la Pareto généralisée complétée.

--- actuar/R/genpareto.R
+++ actuar/R/genpareto.R
@@ -1,26 +1,35 @@
 ### ===== actuar: an R package for Actuarial Science =====
 ###
-### Definition of the {d,p,q,r,m,lev}generalized pareto functions. 
+### Definition of the {d,p,q,r,m,lev}genpareto functions to compute
+### characteristics of the Generalized Pareto distribution.
 ###
 ### See Appendix A of Klugman, Panjer & Willmot, Loss Models, Second
 ### Edition, Wiley, 2004.
 ###
 ### AUTHORS:  Mathieu Pigeon, Vincent Goulet <vincent.goulet@act.ulaval.ca>
 
-dgenpareto <- function(x, shape1, scale, shape2, log = FALSE)
-     .External("do_dpq", "dgenpareto", x, shape1, scale, shape2, log)
+dgenpareto <- function(x, shape1, shape2, rate = 1, scale = 1/rate,
+                       log = FALSE)
+     .External("do_dpq", "dgenpareto", x, shape1, shape2, scale, log)
 
-pgenpareto <- function(q, shape1, scale, shape2, lower.tail = TRUE, log.p = FALSE)
-     .External("do_dpq", "pgenpareto", q, shape1, scale, shape2, lower.tail, log.p) 
+pgenpareto <- function(q, shape1, shape2, rate = 1, scale = 1/rate,
+                       lower.tail = TRUE, log.p = FALSE)
+     .External("do_dpq", "pgenpareto", q, shape1, shape2, scale,
+               lower.tail, log.p)
 
-qgenpareto <- function(p, shape1, scale, shape2, lower.tail = TRUE, log.p = FALSE)
-     .External("do_dpq", "qgenpareto", p, shape1, scale, shape2, lower.tail, log.p) 
+qgenpareto <- function(p, shape1, shape2, rate = 1, scale = 1/rate,
+                       lower.tail = TRUE, log.p = FALSE)
+     .External("do_dpq", "qgenpareto", p, shape1, shape2, scale,
+               lower.tail, log.p)
 
-rgenpareto <- function(n, shape1, scale, shape2)
-     .External("do_random", "rgenpareto", n, shape1, scale, shape2)
+rgenpareto <- function(n, shape1, shape2, rate = 1, scale = 1/rate)
+     .External("do_random", "rgenpareto", n, shape1, shape2, scale)
 
-mgenpareto <- function(k, shape1, scale, shape2, log = FALSE)
-     .External("do_dpq", "mgenpareto", k, shape1, scale, shape2, log)
+mgenpareto <- function(order, shape1, shape2, rate = 1, scale = 1/rate,
+                       log = FALSE)
+     .External("do_dpq", "mgenpareto", order, shape1, shape2, scale, log)
 
-levgenpareto <- function(d, shape1, scale, shape2, order = 1, log = FALSE)
-     .External("do_dpq", "levgenpareto", d, shape1, scale, shape2, order, log)
+levgenpareto <- function(limit, shape1, shape2, rate = 1, scale = 1/rate,
+                         order = 1, log = FALSE)
+     .External("do_dpq", "levgenpareto", limit, shape1, shape2, scale,
+               order, log)
