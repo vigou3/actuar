a4e1184ef7a255955c4b819df91b6c717f5e370e
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-11 20:53:25 +0000

    Révision de l'inverse exponentielle complétée.

--- actuar/R/invexp.R
+++ actuar/R/invexp.R
@@ -1,30 +1,34 @@
 ### ===== actuar: an R package for Actuarial Science =====
 ###
-### Definition of the {d,p,q,r,m,lev}inverse exponential functions. The
-### inverse exponential distribution used in these functions has
-### cumulative distribution function
+### Definition of the {d,p,q,r,m,lev}invexp functions to compute
+### characteristics of the Inverse Exponential distribution. The
+### version used in these functions has cumulative distribution
+### function
 ###
-###   Pr[X <= x] = exp(-(scale/x)), x > 0.
+###   Pr[X <= x] = exp(-scale/x), x > 0.
 ###
 ### See Appendix A of Klugman, Panjer & Willmot, Loss Models, Second
 ### Edition, Wiley, 2004.
 ###
 ### AUTHORS:  Mathieu Pigeon, Vincent Goulet <vincent.goulet@act.ulaval.ca>
 
-dinvexp <- function (x, scale, log = FALSE)
+dinvexp <- function (x, rate = 1, scale = 1/rate, log = FALSE)
     .External("do_dpq", "dinvexp", x, scale, log)
 
-pinvexp <- function(q, scale, lower.tail = TRUE, log.p = FALSE)
+pinvexp <- function(q, rate = 1, scale = 1/rate,
+                    lower.tail = TRUE, log.p = FALSE)
     .External("do_dpq", "pinvexp", q, scale, lower.tail, log.p)
 
-qinvexp <- function(p, scale, lower.tail = TRUE, log.p = FALSE)
+qinvexp <- function(p, rate = 1, scale = 1/rate,
+                    lower.tail = TRUE, log.p = FALSE)
     .External("do_dpq", "qinvexp", p, scale, lower.tail, log.p)
 
-rinvexp <- function(n, scale)
+rinvexp <- function(n, rate = 1, scale = 1/rate)
     .External("do_random", "rinvexp", n, scale)
 
-minvexp <- function(k, scale, log = FALSE)
-    .External("do_dpq", "minvexp", k, scale, log)
+minvexp <- function(order, rate = 1, scale = 1/rate, log = FALSE)
+    .External("do_dpq", "minvexp", order, scale, log)
 
-levinvexp <- function(d, scale, order = 1, log = FALSE)
-    .External("do_dpq", "levinvexp", d, scale, order, log)
+levinvexp <- function(limit, rate = 1, scale = 1/rate,
+                      order = 1, log = FALSE)
+    .External("do_dpq", "levinvexp", limit, scale, order, log)
--- actuar/src/invexp.c
+++ actuar/src/invexp.c
@@ -1,8 +1,9 @@
 /*  ===== actuar: an R package for Actuarial Science =====
  *
- *  Fonctions to compute density, cumulative distribution and quantile
- *  fonctions of the inverse exponential distribution, to calculate raw moments and limited moments 
- *  of the random variable and to simulate random variates. See ../R/invexp.R for details.
+ *  Functions to compute density, cumulative distribution and quantile
+ *  functions, raw and limited moments and to simulate random variates
+ *  for the inverse exponential distribution. See ../R/invexp.R for
+ *  details.
  *
  *  AUTHORS: Mathieu Pigeon and Vincent Goulet <vincent.goulet@act.ulaval.ca>
  */
@@ -14,73 +15,83 @@
 
 double dinvexp(double x, double scale, int give_log)
 {
-  
-  double tmp;
-  
-  if (!R_FINITE(scale) || scale <= 0.0) 
-    return R_NaN;
-  
-  if (!R_FINITE(x) || x < 0.0) 
-    return R_D_d0;
-  
-  tmp = scale / x;
-  
-  return  give_log ?
-    log(scale) - tmp - 2.0 * log (x) :
-    scale * exp(-tmp) / R_pow(x, 2.0);
+    double tmp;
+
+    if (!R_FINITE(scale) || scale <= 0.0)
+	return R_NaN;
+
+    if (!R_FINITE(x) || x < 0.0)
+	return R_D_d0;
+
+    tmp = scale / x;
+
+    return  give_log ?
+	log(scale) - tmp - 2.0 * log (x) :
+	tmp * exp(-tmp) / x;
 }
 
 double pinvexp(double q, double scale, int lower_tail, int log_p)
 {
-  double tmp;
-  
-  if (!R_FINITE(scale) || scale <= 0.0)
-    return R_NaN;
-  
-  if (q <= 0)
-    return R_DT_0;
-  
-  tmp = scale / q;
-  
-  return (lower_tail ? R_D_exp(-tmp):
-	  R_D_exp(log(1.0 - exp(-tmp))));
+    double tmp;
+
+    if (!R_FINITE(scale) || scale <= 0.0)
+	return R_NaN;
+
+    if (q <= 0)
+	return R_DT_0;
+
+    tmp = scale / q;
+
+    return lower_tail ? R_D_exp(-tmp): R_D_exp(log(1.0 - exp(-tmp)));
 }
 
 double qinvexp(double p, double scale, int lower_tail, int log_p)
 {
-  double tmp;
-  
-  if (!R_FINITE(scale) || scale <= 0.0)
-    return R_NaN;
-  
-  R_Q_P01_boundaries(p, 0, R_PosInf);
-  tmp = R_D_qIv(p);
-  
-  return (lower_tail ? -scale / log(tmp) :
-	  -scale / log(1.0 - tmp)) ;
+    double tmp;
+
+    if (!R_FINITE(scale) || scale <= 0.0)
+	return R_NaN;
+
+    R_Q_P01_boundaries(p, 0, R_PosInf);
+    tmp = R_D_qIv(p);
+
+    return lower_tail ? -scale / log(tmp) : -scale / log(1.0 - tmp);
 }
 
 
 double rinvexp(double scale)
 {
-  if (!R_FINITE(scale) || scale <= 0.0)
-    return R_NaN;
-  
-  return scale / log(1.0 / unif_rand());
+    if (!R_FINITE(scale) || scale <= 0.0)
+	return R_NaN;
+
+    return -scale / log(unif_rand());
 }
 
-double minvexp(double k, double scale, int give_log)
+double minvexp(double order, double scale, int give_log)
 {
-  if (!R_FINITE(scale) || !R_FINITE(k) || scale <= 0.0 || k >= 1.0)
-    return R_NaN;
-  
-  return R_pow(scale, k) * gammafn(1.0 - k);
+    if (!R_FINITE(scale) ||
+	!R_FINITE(order) ||
+	scale <= 0.0 ||
+	order >= 1.0)
+	return R_NaN;
+
+    return R_pow(scale, order) * gammafn(1.0 - order);
 }
 
-double levinvexp(double d, double scale, double order, int give_log)
+double levinvexp(double limit, double scale, double order, int give_log)
 {
-  if (!R_FINITE(scale) || !R_FINITE(d) || R_FINITE(order) || scale <= 0.0 || order <= 0.0 || order >= 1.0)
-    return R_NaN;
-  
-  return R_pow(scale, order) * gammafn(1.0 - order) * (1.0 - pgamma(1.0 / d, 1.0 - order, 1.0 / scale, 1, 0)) + R_pow(d, order) * (1.0 - exp(-scale / d));
+    double tmp;
+
+    if (!R_FINITE(scale) ||
+	!R_FINITE(limit) ||
+	R_FINITE(order) ||
+	scale <= 0.0 ||
+	order <= 0.0 ||
+	order >= 1.0 ||
+	limit <= 0.0)
+	return R_NaN;
+
+    tmp = scale / limit;
+
+    return R_pow(scale, order) * gammafn(1.0 - order) * pgamma(tmp, 1.0 - order, 1.0, 0, 0) + R_pow(limit, order) * (1.0 - exp(-tmp));
 }
