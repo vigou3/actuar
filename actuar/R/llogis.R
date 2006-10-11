232d10f8526c4c2cb0244bf1730edf3fb2f79c46
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-11 02:38:54 +0000

    Ajout du paramètre 'rate'.

--- actuar/R/llogis.R
+++ actuar/R/llogis.R
@@ -11,20 +11,23 @@
 ###
 ### AUTHORS:  Mathieu Pigeon, Vincent Goulet <vincent.goulet@act.ulaval.ca>
 
-dllogis <- function (x, shape, scale, log = FALSE)
+dllogis <- function (x, shape, rate = 1, scale = 1/rate, log = FALSE)
      .External("do_dpq", "dllogis", x, shape, scale, log)
 
-pllogis <- function(q, shape, scale, lower.tail = TRUE, log.p = FALSE)
+pllogis <- function(q, shape, rate = 1, scale = 1/rate,
+                    lower.tail = TRUE, log.p = FALSE)
      .External("do_dpq", "pllogis", q, shape, scale, lower.tail, log.p)
 
-qllogis <- function(p, shape, scale, lower.tail = TRUE, log.p = FALSE)
+qllogis <- function(p, shape, rate = 1, scale = 1/rate,
+                    lower.tail = TRUE, log.p = FALSE)
      .External("do_dpq", "qllogis", p, shape, scale, lower.tail, log.p)
 
-rllogis <- function(n, shape, scale)
+rllogis <- function(n, shape, rate = 1, scale = 1/rate)
      .External("do_random", "rllogis", n, shape, scale)
 
-mllogis <- function(order, shape, scale, log = FALSE)
+mllogis <- function(order, shape, rate = 1, scale = 1/rate, log = FALSE)
      .External("do_dpq", "mllogis", order, shape, scale, log)
 
-levllogis <- function(limit, shape, scale, order = 1, log = FALSE)
+levllogis <- function(limit, shape, rate = 1, scale = 1/rate,
+                      order = 1, log = FALSE)
      .External("do_dpq", "levllogis", limit, shape, scale, order, log)
