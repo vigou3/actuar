325acbda79af7cb9adec78d4000aecf6f37e7315
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-11 02:51:14 +0000

    Révision de la Paralogistique complétée.

--- actuar/R/paralogis.R
+++ actuar/R/paralogis.R
@@ -1,8 +1,8 @@
 ### ===== actuar: an R package for Actuarial Science =====
 ###
-### Definition of the {d,p,q,r,m,lev}paralogistic functions. The paralogistic
-### distribution used in these functions has cumulative distribution
-### function
+### Definition of the {d,p,q,r,m,lev}paralogis functions to compute
+### characteristics of the paralogistic distribution. The version used
+### in these functions has cumulative distribution function
 ###
 ###   Pr[X <= x] = 1 - (1/(1 + (x/scale)^shape))^shape, x > 0.
 ###
@@ -11,20 +11,23 @@
 ###
 ### AUTHORS:  Mathieu Pigeon, Vincent Goulet <vincent.goulet@act.ulaval.ca>
 
-dparalogis <- function (x, shape, scale, log = FALSE)
+dparalogis <- function (x, shape, rate = 1, scale = 1/rate, log = FALSE)
     .External("do_dpq", "dparalogis", x, shape, scale, log)
 
-pparalogis <- function(q, shape, scale, lower.tail = TRUE, log.p = FALSE)
+pparalogis <- function(q, shape, rate = 1, scale = 1/rate,
+                       lower.tail = TRUE, log.p = FALSE)
     .External("do_dpq", "pparalogis", q, shape, scale, lower.tail, log.p)
 
-qparalogis <- function(p, shape, scale, lower.tail = TRUE, log.p = FALSE)
+qparalogis <- function(p, shape, rate = 1, scale = 1/rate,
+                       lower.tail = TRUE, log.p = FALSE)
     .External("do_dpq", "qparalogis", p, shape, scale, lower.tail, log.p)
 
-rparalogis <- function(n, shape, scale)
+rparalogis <- function(n, shape, rate = 1, scale = 1/rate)
     .External("do_random", "rparalogis", n, shape, scale)
 
-mparalogis <- function(k, shape, scale, log = FALSE)
-    .External("do_dpq", "mparalogis", k, shape, scale, log)
+mparalogis <- function(order, shape, rate = 1, scale = 1/rate, log = FALSE)
+    .External("do_dpq", "mparalogis", order, shape, scale, log)
 
-levparalogis <- function(d, shape, scale, order = 1, log = FALSE)
-    .External("do_dpq", "levparalogis", d, shape, scale, order, log)
+levparalogis <- function(limit, shape, rate = 1, scale = 1/rate,
+                         order = 1, log = FALSE)
+    .External("do_dpq", "levparalogis", limit, shape, scale, order, log)
--- actuar/src/paralogis.c
+++ actuar/src/paralogis.c
@@ -1,8 +1,9 @@
 /*  ===== actuar: an R package for Actuarial Science =====
  *
- *  Fonctions to compute density, cumulative distribution and quantile
- *  fonctions of the paralogistic distribution, to calculate raw moments and limited moments 
- *  of the random variable and to simulate random variates. See ../R/paralogis.R for details.
+ *  Functions to compute density, cumulative distribution and quantile
+ *  functions, raw and limited moments and to simulate random variates
+ *  for the paralogistic distribution. See ../R/paralogis.R for
+ *  details.
  *
  *  AUTHORS: Mathieu Pigeon and Vincent Goulet <vincent.goulet@act.ulaval.ca>
  */
@@ -14,111 +15,117 @@
 
 double dparalogis(double x, double shape, double scale, int give_log)
 {
-  double tmp1, tmp2;
-  
-  if (!R_FINITE(shape) ||
-      !R_FINITE(scale) ||
-      shape <= 0.0 || 
-      scale <= 0.0) 
-    return R_NaN;
-  
-  if (!R_FINITE(x) || x < 0.0) 
-    return R_D_d0;
-  
-  tmp1 = log(x) - log(scale);
-  tmp2 = R_pow(x / scale, shape);
-  
-  return  give_log ?
-    2.0 * log(shape) + shape * tmp1 - log(x) - (shape + 1.0) * log(1.0 + exp(shape * tmp1)) :
-    R_pow(shape, 2.0) * tmp2 / (x * R_pow(1.0 + tmp2, shape + 1.0));
+    double tmp1, tmp2;
+
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	shape <= 0.0 ||
+	scale <= 0.0)
+	return R_NaN;
+
+    if (!R_FINITE(x) || x < 0.0)
+	return R_D_d0;
+
+    tmp1 = shape * (log(x) - log(scale));
+    tmp2 = R_pow(x / scale, shape);
+
+    return  give_log ?
+	2.0 * log(shape) + tmp1 - log(x) - (shape + 1.0) * log(1.0 + tmp2) :
+	R_pow(shape, 2.0) * tmp2 / (x * R_pow(1.0 + tmp2, shape + 1.0));
 }
 
 double pparalogis(double x, double shape, double scale, int lower_tail, int log_p)
 {
-  
-  double tmp1, tmp2;
-  
-  if (!R_FINITE(shape) ||
-      !R_FINITE(scale) ||
-      shape <= 0.0 || 
-      scale <= 0.0) 
-    return R_NaN;
+    double tmp1, tmp2;
 
-  if (x <= 0)
-    return R_DT_0;
-  
-  tmp1 = R_pow(scale, shape);
-  tmp2 = R_pow(x, shape);
-  
-  return (lower_tail ? R_D_exp(log(1.0 - exp(shape * ( shape * log(scale) - log(tmp1 + tmp2))))):
-	  R_D_exp(shape * ( shape * log(scale) - log(tmp1 + tmp2))));
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	shape <= 0.0 ||
+	scale <= 0.0)
+	return R_NaN;
+
+    if (x <= 0)
+	return R_DT_0;
+
+    tmp1 = R_pow(scale, shape);
+    tmp2 = R_pow(x, shape);
+
+    return lower_tail ?
+	R_D_exp(log(1.0 - exp(shape * (shape * log(scale) - log(tmp1 + tmp2))))):
+	R_D_exp(shape * (shape * log(scale) - log(tmp1 + tmp2)));
 }
 
 double qparalogis(double p, double shape, double scale, int lower_tail, int log_p)
 {
-  
-  double tmp, tmp1;
-  
-  if (!R_FINITE(shape) ||
-      !R_FINITE(scale) ||
-      shape <= 0.0 || 
-      scale <= 0.0)
-    return R_NaN;
-  
-  R_Q_P01_boundaries(p, 0, R_PosInf);
-  tmp = R_D_qIv(p);
-  tmp1 = 1.0 / shape;
-  
-  return (lower_tail ? scale * R_pow(R_pow(1.0 - tmp, -tmp1) - 1.0, tmp1) :
-	  scale * R_pow(R_pow(tmp, -tmp1) - 1.0, tmp1));
+    double tmp, tmp1;
+
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	shape <= 0.0 ||
+	scale <= 0.0)
+	return R_NaN;
+
+    R_Q_P01_boundaries(p, 0, R_PosInf);
+    tmp = R_D_qIv(p);
+
+    tmp1 = 1.0 / shape;
+
+    return lower_tail ? scale * R_pow(R_pow(1.0 - tmp, -tmp1) - 1.0, tmp1) :
+	scale * R_pow(R_pow(tmp, -tmp1) - 1.0, tmp1);
 }
 
 double rparalogis(double shape, double scale)
 {
-  double a;
-  
-  if (!R_FINITE(shape) ||
-      !R_FINITE(scale) ||
-      shape <= 0.0 ||
-      scale <= 0.0)
-    return R_NaN;
-  
-  a =  R_pow(unif_rand(), 1.0 / shape);
-  
-  return scale * R_pow((1.0 - a) / a, 1.0 / shape);
+    double a, tmp;
+
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	shape <= 0.0 ||
+	scale <= 0.0)
+	return R_NaN;
+
+    tmp = 1.0 / shape;
+
+    a =  unif_rand();
+
+    return scale * R_pow(R_pow(a, -tmp) - 1.0, tmp);
 }
 
-double mparalogis(double k, double shape, double scale, int give_log)
+double mparalogis(double order, double shape, double scale, int give_log)
 {
-  
-  if (!R_FINITE(shape) ||
-      !R_FINITE(scale) ||
-      !R_FINITE(k) ||
-      shape <= 0.0 ||
-      scale <= 0.0 ||
-      k <= -shape ||
-      k >= R_pow(shape, 2.0))
-    return R_NaN;
-  
-  return R_pow(scale, k) * gammafn(1.0 + k / shape) * gammafn(shape - k / shape) / gammafn(shape);
+    double tmp;
+
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	!R_FINITE(order) ||
+	shape <= 0.0 ||
+	scale <= 0.0 ||
+	order <= -shape ||
+	order >= R_pow(shape, 2.0))
+	return R_NaN;
+
+    tmp = order / shape;
+
+    return R_pow(scale, order) * gammafn(1.0 + tmp) * gammafn(shape - tmp) / gammafn(shape);
 }
 
-double levparalogis(double d, double shape, double scale, double order, int give_log)
+double levparalogis(double limit, double shape, double scale, double order, int give_log)
 {
-  double temp;
-  
-  if (!R_FINITE(shape) ||
-      !R_FINITE(scale) ||
-      !R_FINITE(d) ||
-      !R_FINITE(order) ||
-      shape <= 0.0 ||
-      scale <= 0.0 ||
-      order <= -shape ||
-      order >= R_pow(shape, 2.0) ||
-      d <= 0.0)
-    return R_NaN;
-  
-  temp = 1.0 / (1.0 + R_pow(d / scale, shape));
-  
-  return R_pow(scale, order) * gammafn(1.0 + order / shape) * gammafn(shape - order / shape) * pbeta(1.0 - temp, 1.0 + order / shape, shape - order / shape, 1, 0) + R_pow(d, order) * R_pow(temp, shape);
+    double u, tmp;
+
+    if (!R_FINITE(shape) ||
+	!R_FINITE(scale) ||
+	!R_FINITE(limit) ||
+	!R_FINITE(order) ||
+	shape <= 0.0 ||
+	scale <= 0.0 ||
+	order <= -shape ||
+	order >= R_pow(shape, 2.0) ||
+	limit <= 0.0)
+	return R_NaN;
+
+    tmp = order / shape;
+    u = 1.0 / (1.0 + R_pow(limit / scale, shape));
+
+    return R_pow(scale, order) * gammafn(1.0 + tmp) * gammafn(shape - tmp) * pbeta(1.0 - u, 1.0 + tmp, shape - tmp, 1, 0) + R_pow(limit, order) * R_pow(u, shape);
 }
