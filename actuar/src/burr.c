e8d5ffb15b41a81d20d9fbf2e4e0de9dbeb1dd5d
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-13 15:23:34 +0000

    Optimisation et simplification de burr.c (pas testé).

--- actuar/src/burr.c
+++ actuar/src/burr.c
@@ -12,131 +12,137 @@
 #include "locale.h"
 #include "dpq.h"
 
-double dburr(double x, double shape1, double shape2, double scale, int give_log)
+double dburr(double x, double shape1, double shape2, double scale,
+	     int give_log)
+{
+    /*  We work with the density expressed as
+     *
+     *  shape1 * shape2 * u * (1 - u) / x
+     *
+     *  with u = v/(1 + v), v = (x/scale)^shape2.
+     */
+
+    double tmp, logu, log1mu;
+
+    if (!R_FINITE(shape1) ||
+	!R_FINITE(shape2) ||
+	!R_FINITE(scale)  ||
+	shape1 <= 0.0 ||
+	shape2 <= 0.0 ||
+	scale  <= 0.0)
+	return R_NaN;
+
+    if (!R_FINITE(x) || x < 0.0)
+	return R_D_d0;
+
+    tmp = shape2 * (log(x) - log(scale));
+    log1mu = - log1p(exp(tmp));
+    logu = tmp + log1mu;
+
+    return R_D_exp(log(shape1) + log(shape2) + logu + log1mu - log(x));
+}
+
+double pburr(double q, double shape1, double shape2, double scale,
+	     int lower_tail, int log_p)
 {
     double tmp;
 
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
+	!R_FINITE(scale)  ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
-	shape2 <= 0.0)
+	shape2 <= 0.0 ||
+	scale  <= 0.0)
 	return R_NaN;
 
-    if (!R_FINITE(x)  ||
-	x < 0.0)
-	return R_D_d0;
-
-    tmp = R_pow(x / scale, shape2);
-
-    return give_log ?
-	log(shape1) + log(shape2) + shape2 * (log(x) - log(scale)) - log(x) - (shape1 + 1.0) * (log(R_pow(scale, shape2) + R_pow(x, shape2)) - shape2 * log(scale)) :
-	shape1 * shape2 * tmp / (x * R_pow(1.0 + tmp, shape1 + 1.0));
-}
-
-double pburr(double q, double shape1, double shape2, double scale, int lower_tail, int log_p)
-{
-    double tmp1, tmp2;
-
-    if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
-	!R_FINITE(shape2) ||
-	shape1 <= 0.0 ||
-	scale <= 0.0 ||
-	shape2 <= 0.0)
-	return R_NaN;
     if (q <= 0)
 	return R_DT_0;
 
-    tmp1 = R_pow(scale, shape2);
-    tmp2 = R_pow(q, shape2);
+    tmp = exp(shape2 * (log(q) - log(scale)));
 
-    return lower_tail ?
-	R_D_exp(log(1.0 - exp(shape1 * (shape2 * log(scale) - log(tmp1 + tmp2))))):
-	R_D_exp(shape1 * (shape2 * log(scale) - log(tmp1 + tmp2)));
+    return R_DT_Cval(R_pow(1.0 + tmp, -shape1));
 }
 
-double qburr(double p, double shape1, double shape2, double scale, int lower_tail, int log_p)
+double qburr(double p, double shape1, double shape2, double scale,
+	     int lower_tail, int log_p)
 {
-    double tmp, tmp1, tmp2;
-
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
+	!R_FINITE(scale)  ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
-	shape2 <= 0.0)
+	shape2 <= 0.0 ||
+	scale  <= 0.0)
 	return R_NaN;
 
     R_Q_P01_boundaries(p, 0, R_PosInf );
-    tmp =  R_D_qIv(p);
+    p =  R_D_qIv(p);
 
-    tmp1 = 1.0 / shape1;
-    tmp2 = 1.0 / shape2;
-
-    return lower_tail ?
-	scale * R_pow((1.0 - R_pow(1.0 - tmp, tmp1)) / R_pow(1.0 - tmp, tmp1), tmp2) :
-	scale * R_pow((1.0 - R_pow(tmp, tmp1)) / R_pow(tmp, tmp1), tmp2);
+    return scale * R_pow(R_pow(R_D_Cval(p), -1.0/shape1) - 1.0, 1.0/shape2);
 }
 
 double rburr(double shape1, double shape2, double scale)
 {
-    double a, tmp;
-
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
+	!R_FINITE(scale)  ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
-	shape2 <= 0.0)
+	shape2 <= 0.0 ||
+	scale  <= 0.0)
 	return R_NaN;
 
-    a = unif_rand();
-    tmp = R_pow(a, -1.0 / shape1);
-
-    return scale * R_pow(tmp, 1.0 / shape2);
+    return scale * R_pow(R_pow(unif_rand(), -1.0/shape1) - 1.0, 1.0/shape2);
 }
 
-double mburr(double order, double shape1, double shape2, double scale, int give_log)
+double mburr(double order, double shape1, double shape2, double scale,
+	     int give_log)
 {
     double tmp;
 
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
+	!R_FINITE(scale)  ||
 	!R_FINITE(order) ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
 	shape2 <= 0.0 ||
-	order <= -shape2 ||
-	order >= shape1 * shape2)
+	scale  <= 0.0 ||
+	order  <= -shape2 ||
+	order  >= shape1 * shape2)
 	return R_NaN;
 
     tmp = order / shape2;
 
-    return R_pow(scale, order) * gammafn(1.0 + tmp) * gammafn(shape1 - tmp) / gammafn(shape1);
+    return R_pow(scale, order) * gammafn(1.0 + tmp) * gammafn(shape1 - tmp)
+	/ gammafn(shape1);
 }
 
-double levburr(double limit, double shape1, double shape2, double scale, double order, int give_log)
+double levburr(double limit, double shape1, double shape2, double scale,
+	       double order, int give_log)
 {
-    double u, tmp;
+    double u, tmp1, tmp2, tmp3;
 
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
-	!R_FINITE(limit) ||
+	!R_FINITE(scale)  ||
 	!R_FINITE(order) ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
 	shape2 <= 0.0 ||
-	limit <= 0.0 ||
-	order <= -shape2 ||
-	order >= shape1 * shape2)
+	scale  <= 0.0 ||
+	order  <= -shape2)
 	return R_NaN;
 
-    u = 1.0 / (1.0 + R_pow(limit / scale, shape2));
-    tmp = order / shape2;
+    if (limit <= 0.0)
+	return 0;
 
-    return R_pow(scale, order) * gammafn(1.0 + tmp) * gammafn(shape1 - tmp) * pbeta(1.0 - u, 1.0 + tmp, shape1 - tmp, 1, 0) / gammafn(shape1) + R_pow(limit, order) * R_pow(u, shape1);
+    if (!R_FINITE(limit))
+	return mburr(order, shape1, shape2, scale, 0);
+
+    tmp1 = order / shape2;
+    tmp2 = 1.0 + tmp1;
+    tmp3 = shape1 - tmp1;
+
+    u = 1.0 / (1.0 + exp(shape2 * (log(limit) - log(scale))));
+
+    return R_pow(scale, order) * gammafn(tmp2) * gammafn(tmp3)
+	* pbeta(1.0 - u, tmp2, tmp3, 1, 0) / gammafn(shape1)
+	+ R_pow(limit, order) * R_pow(u, shape1);
 }
