0b5ca26ed6e1d96d2782189acb0cd8367100d19d
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-13 21:43:14 +0000

    Introduction d'une méthode de 'mean' pour les données groupées.

---
+++ actuar/R/mean.grouped.data.R
@@ -0,0 +1,20 @@
+### ===== actuar: an R package for Actuarial Science =====
+###
+### Mean (TODO: and summaries) of grouped data objects
+###
+### See Klugman, Panjer & Willmot, Loss Models, Wiley, 1998.
+###
+### AUTHOR: Vincent Goulet <vincent.goulet@act.ulaval.ca>
+
+mean.grouped.data <- function(x, ...)
+{
+    ## Sanity check
+    if (!inherits(x, "grouped.data"))
+        stop("wrong method")
+
+    cj <- eval(expression(cj), env = environment(x))
+
+    midpoints <- cj[-length(cj)] + diff(cj)/2
+
+    sapply(x[, -1], function(x) drop(crossprod(x, midpoints)))
+}
