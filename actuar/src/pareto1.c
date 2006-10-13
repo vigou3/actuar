ea1fe26b82971c09749d7d7c16b37f321b028cd0
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-13 21:29:58 +0000

    Optimisation et simplification de pareto1.c (pas testé).

--- actuar/src/pareto1.c
+++ actuar/src/pareto1.c
@@ -24,15 +24,11 @@ double dpareto1(double x, double shape, double min, int give_log)
     if (!R_FINITE(x) || x < min)
 	return R_D_d0;
 
-    return give_log ?
-	log(shape) + shape * log(min) - (shape + 1.0) * log(x):
-	shape * R_pow(min, shape) / R_pow(x, shape + 1.0);
+    return R_D_exp(log(shape) + shape * log(min) - (shape + 1.0) * log(x));
 }
 
 double ppareto1(double q, double shape, double min, int lower_tail, int log_p)
 {
-    double tmp;
-
     if (!R_FINITE(shape) ||
 	!R_FINITE(min) ||
 	shape <= 0.0 ||
@@ -42,17 +38,11 @@ double ppareto1(double q, double shape, double min, int lower_tail, int log_p)
     if (q <= min)
 	return R_DT_0;
 
-    tmp = shape * (log(min) - log(q));
-
-    return lower_tail ? R_D_exp(log(1 - exp(tmp))):
-	R_D_exp(tmp);
+    return R_DT_Cval(R_pow(min / q, shape));
 }
 
 double qpareto1(double p, double shape, double min, int lower_tail, int log_p)
 {
-
-    double tmp, tmp1;
-
     if (!R_FINITE(shape) ||
 	!R_FINITE(min) ||
 	shape <= 0.0 ||
@@ -60,11 +50,9 @@ double qpareto1(double p, double shape, double min, int lower_tail, int log_p)
 	return R_NaN;
 
     R_Q_P01_boundaries(p, min, R_PosInf);
-    tmp = R_D_qIv(p);
-    tmp1 = 1.0 / shape;
+    p = R_D_qIv(p);
 
-    return lower_tail ? min / R_pow(1.0 - tmp, tmp1) :
-	min / R_pow(tmp, tmp1);
+    return min / R_pow(R_D_Cval(p), 1.0 / shape);
 }
 
 double rpareto1(double shape, double min)
@@ -91,21 +79,20 @@ double mpareto1(double order, double shape, double min, int give_log)
     return shape * R_pow(min, order) / (shape - order);
 }
 
-double levpareto1(double limit, double shape, double min, double order, int give_log)
+double levpareto1(double limit, double shape, double min, double order,
+		  int give_log)
 {
     double tmp;
 
     if (!R_FINITE(shape) ||
 	!R_FINITE(min) ||
-	!R_FINITE(limit) ||
 	!R_FINITE(order) ||
 	shape <= 0.0 ||
-	min <= 0.0 ||
-	order >= shape ||
-	limit <= 0.0)
+	min <= 0.0)
 	return R_NaN;
 
     tmp = shape - order;
 
-    return shape * R_pow(min, order) / tmp - order * R_pow(min, shape) / (tmp * R_pow(limit, tmp));
+    return shape * R_pow(min, order) / tmp
+	- order * R_pow(min, shape) / (tmp * R_pow(limit, tmp));
 }
