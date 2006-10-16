887a2b309a3bdf9928010236f91be0624dd97c47
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-16 03:53:24 +0000

    Simplification et optimisation de la famille inverse gamma transformée.

--- actuar/src/invweibull.c
+++ actuar/src/invweibull.c
@@ -15,10 +15,17 @@
 
 double dinvweibull(double x, double shape, double scale, int give_log)
 {
-    double tmp;
+    /*  We work with the density expressed as
+     *
+     *  shape * u * e^(-u) / x
+     *
+     *  with u = (scale/x)^shape.
+     */
+
+    double logu;
 
     if (!R_FINITE(scale) ||
-	!R_FINITE(shape)  ||
+	!R_FINITE(shape) ||
 	scale <= 0.0 ||
 	shape <= 0.0)
 	return R_NaN;;
@@ -26,16 +33,15 @@ double dinvweibull(double x, double shape, double scale, int give_log)
     if (!R_FINITE(x) || x < 0.0)
 	return R_D_d0;
 
-    tmp = R_pow(scale / x, shape);
+    logu = shape * (log(scale) - log(x));
 
-    return  give_log ?
-	log(shape) + shape * (log(scale) - log(x)) - tmp - log(x) :
-	shape * tmp * exp(-tmp) / x;
+    return R_D_exp(log(shape) + logu - exp(logu) - log(x));
 }
 
-double pinvweibull(double q, double shape, double scale, int lower_tail, int log_p)
+double pinvweibull(double q, double shape, double scale, int lower_tail,
+		   int log_p)
 {
-    double tmp;
+    double u;
 
     if (!R_FINITE(scale) ||
 	!R_FINITE(shape) ||
@@ -46,15 +52,14 @@ double pinvweibull(double q, double shape, double scale, int lower_tail, int log
     if (q <= 0)
 	return R_DT_0;
 
-    tmp = R_pow(scale / q, shape);
+    u = exp(shape * (log(scale) - log(q)));
 
-    return lower_tail ? R_D_exp(-tmp): R_D_exp(log(1.0 - exp(-tmp)));
+    return R_DT_val(exp(-u));
 }
 
-double qinvweibull(double p, double shape, double scale, int lower_tail, int log_p)
+double qinvweibull(double p, double shape, double scale, int lower_tail,
+		   int log_p)
 {
-    double tmp, tmp1;
-
     if (!R_FINITE(scale) ||
 	!R_FINITE(shape) ||
 	scale <= 0.0 ||
@@ -62,13 +67,9 @@ double qinvweibull(double p, double shape, double scale, int lower_tail, int log
 	return R_NaN;;
 
     R_Q_P01_boundaries(p, 0, R_PosInf);
-    tmp = R_D_qIv(p);
+    p = R_D_qIv(p);
 
-    tmp1 = -1.0 / shape;
-
-    return lower_tail ?
-	scale * R_pow(-log(tmp), tmp1):
-	scale * R_pow(-log(1.0 - tmp), tmp1);
+    return scale * R_pow(-log(R_D_Lval(p)), -1.0 / shape);
 }
 
 double rinvweibull(double shape, double scale)
@@ -79,7 +80,7 @@ double rinvweibull(double shape, double scale)
 	shape <= 0.0)
 	return R_NaN;;
 
-    return scale * R_pow(-log(unif_rand()), -1.0/shape);
+    return scale * R_pow(rexp(1.0), -1.0 / shape);
 }
 
 double minvweibull(double order, double shape, double scale, int give_log)
@@ -95,22 +96,26 @@ double minvweibull(double order, double shape, double scale, int give_log)
     return R_pow(scale, order) * gammafn(1.0 - order / shape);
 }
 
-double levinvweibull(double limit, double shape, double scale, double order, int give_log)
+double levinvweibull(double limit, double shape, double scale, double order,
+		     int give_log)
 {
-    double tmp1, tmp2;
+    double u, tmp;
 
     if (!R_FINITE(scale) ||
 	!R_FINITE(shape) ||
-	!R_FINITE(limit) ||
 	!R_FINITE(order) ||
 	scale <= 0.0 ||
 	shape <= 0.0 ||
-	order >= shape ||
-	limit <= 0.0)
+	order >= shape)
 	return R_NaN;;
 
-    tmp1 = R_pow(scale / limit, shape);
-    tmp2 = order / shape;
+    if (limit <= 0.0)
+	return 0;
 
-    return R_pow(scale, order) * gammafn(1.0 - tmp2) * pgamma(tmp1, 1.0 - tmp2, 1.0, 0, 0) + R_pow(limit, order) * (1.0 - exp(-tmp1));
+    tmp = 1.0 - order / shape;
+
+    u = exp(shape * (log(scale) - log(limit)));
+
+    return R_pow(scale, order) * gammafn(tmp) * pgamma(u, tmp, 1.0, 0, 0)
+	+ R_pow(limit, order) * (0.5 - exp(-u) + 0.5);
 }
