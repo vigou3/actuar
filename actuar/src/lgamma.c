b086cf4dcbbdb5fd6cac5949b35cd9c4f453947c
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-16 04:13:13 +0000

    Simplification de dgamma(); reformatage du code autrement.

--- actuar/src/lgamma.c
+++ actuar/src/lgamma.c
@@ -23,12 +23,11 @@ double dlgamma(double x, double shapelog, double ratelog, int give_log)
     if (!R_FINITE(x) || x < 1.0)
 	return R_D_d0;
 
-    return give_log ?
-	-log(x) + dgamma(log(x), shapelog, 1.0 / ratelog, 1) :
-	dgamma(log(x), shapelog, 1.0 / ratelog, 0) / x;
+    return R_D_exp(dgamma(log(x), shapelog, 1.0 / ratelog, 1) - log(x));
 }
 
-double plgamma(double q, double shapelog, double ratelog, int lower_tail, int log_p)
+double plgamma(double q, double shapelog, double ratelog, int lower_tail,
+	       int log_p)
 {
     if (!R_FINITE(shapelog) ||
 	!R_FINITE(ratelog) ||
@@ -42,7 +41,8 @@ double plgamma(double q, double shapelog, double ratelog, int lower_tail, int lo
     return pgamma(log(q), shapelog, 1.0 / ratelog, lower_tail, log_p);
 }
 
-double qlgamma(double p, double shapelog, double ratelog, int lower_tail, int log_p)
+double qlgamma(double p, double shapelog, double ratelog, int lower_tail,
+	       int log_p)
 {
     if (!R_FINITE(shapelog) ||
 	!R_FINITE(ratelog) ||
@@ -80,7 +80,8 @@ double mlgamma(double order, double shapelog, double ratelog, int give_log)
     return R_pow(1.0 - order / ratelog, -shapelog);
 }
 
-double levlgamma(double limit, double shapelog, double ratelog, double order, int give_log)
+double levlgamma(double limit, double shapelog, double ratelog, double order,
+		 int give_log)
 {
     double u;
 
@@ -96,5 +97,7 @@ double levlgamma(double limit, double shapelog, double ratelog, double order, in
 
     u = log(limit);
 
-    return R_pow(1.0 - order / ratelog, -shapelog) * pgamma(u * (ratelog - order), shapelog, 1.0, 1, 0) + R_pow(limit, order) * pgamma(u * ratelog, shapelog, 1.0, 0, 0);
+    return R_pow(1.0 - order / ratelog, -shapelog)
+	* pgamma(u * (ratelog - order), shapelog, 1.0, 1, 0)
+	+ R_pow(limit, order) * pgamma(u * ratelog, shapelog, 1.0, 0, 0);
 }
