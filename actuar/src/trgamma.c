7fa29e91d0af461b5e8726227110b6450e131ccf
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-16 00:54:11 +0000

    Simplification et optimisation de trgamma.c

--- actuar/src/trgamma.c
+++ actuar/src/trgamma.c
@@ -13,128 +13,127 @@
 #include "locale.h"
 #include "dpq.h"
 
-double dtrgamma(double x, double shape1, double shape2, double scale, int give_log)
+double dtrgamma(double x, double shape1, double shape2, double scale,
+		int give_log)
 {
-    double tmp1, tmp2;
+    /*  We work with the density expressed as
+     *
+     *  shape2 * u^shape1 * e^(-u) / (x * gamma(shape1))
+     *
+     *  with u = (x/scale)^shape2.
+     */
+
+    double logu;
 
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
+	!R_FINITE(scale)  ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
-	shape2 <= 0.0)
+	shape2 <= 0.0 ||
+	scale  <= 0.0)
 	return R_NaN;
 
     if (!R_FINITE(x) || x < 0.0)
 	return R_D_d0;
 
-    tmp1 = R_pow(x, shape2);
-    tmp2 = R_pow(scale, shape2);
+    logu = shape2 * (log(x) - log(scale));
 
-    return give_log ?
-	dgamma(tmp1, shape1, tmp2, 1) + log(shape2) + (shape2 - 1.0) * log(x) :
-	shape2 * R_pow(x, shape2 - 1.0) * dgamma(tmp1, shape1, tmp2, 0);
+    return R_D_exp(log(shape2) + shape1 * logu - exp(logu)
+		   - log(x) - lgamma(shape1));
 }
 
-double ptrgamma(double q, double shape1, double shape2, double scale, int lower_tail, int log_p)
+double ptrgamma(double q, double shape1, double shape2, double scale,
+		int lower_tail, int log_p)
 {
-    double tmp1, tmp2;
+    double u;
 
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
+	!R_FINITE(scale)  ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
-	shape2 <= 0.0)
+	shape2 <= 0.0 ||
+	scale  <= 0.0)
 	return R_NaN;
 
     if (q <= 0)
 	return R_DT_0;
 
-    if (!R_FINITE(q))
-	return 1;
+    u = exp(shape2 * (log(q) - log(scale)));
 
-    tmp1 = R_pow(q, shape2);
-    tmp2 = R_pow(scale, shape2);
-
-    return lower_tail ?
-	R_D_exp(pgamma(tmp1, shape1, tmp2, 1, 1)):
-	R_D_exp(pgamma(tmp1, shape1, tmp2, 0, 1));
+    return pgamma(u, shape1, 1.0, lower_tail, log_p);
 }
 
-double qtrgamma(double p, double shape1, double shape2, double scale, int lower_tail, int log_p)
+double qtrgamma(double p, double shape1, double shape2, double scale,
+		int lower_tail, int log_p)
 {
-    double tmp1, tmp2, tmp3;
-
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
+	!R_FINITE(scale)  ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
-	shape2 <= 0.0)
+	shape2 <= 0.0 ||
+	scale  <= 0.0)
 	return R_NaN;
 
     R_Q_P01_boundaries(p, 0, R_PosInf);
-    tmp1 = R_D_qIv(p);
-    tmp2 = R_pow(scale, shape2);
-    tmp3 = 1.0 / shape2;
+    p = R_D_qIv(p);
 
-    return lower_tail ?
-	R_pow(qgamma(tmp1, shape1, tmp2, 1, 0), tmp3) :
-	R_pow(qgamma(tmp1, shape1, tmp2, 0, 0), tmp3);
+    return scale * R_pow(qgamma(p, shape1, 1.0, lower_tail, 0),
+			 1.0 / shape2);
 }
 
 double rtrgamma(double shape1, double shape2, double scale)
 {
-    double a;
-
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
+	!R_FINITE(scale)  ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
-	shape2 <= 0.0)
+	shape2 <= 0.0 ||
+	scale  <= 0.0)
 	return R_NaN;
 
-    a = rgamma(shape1, 1.0);
-
-    return scale * R_pow(a, 1.0 / shape2);
+    return scale * R_pow(rgamma(shape1, 1.0), 1.0 / shape2);
 }
 
-double mtrgamma(double order, double shape1, double shape2, double scale, int give_log)
+double mtrgamma(double order, double shape1, double shape2, double scale,
+		int give_log)
 {
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
+	!R_FINITE(scale) ||
 	!R_FINITE(order) ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
 	shape2 <= 0.0 ||
+	scale <= 0.0 ||
 	order <= -shape1 * shape2)
 	return R_NaN;
 
-    return R_pow(scale, order) * gammafn(shape1 + order/shape2) / gammafn(shape1);
+    return R_pow(scale, order) * gammafn(shape1 + order/shape2)
+	/ gammafn(shape1);
 }
 
-double levtrgamma(double limit, double shape1, double shape2, double scale, double order, int give_log)
+double levtrgamma(double limit, double shape1, double shape2, double scale,
+		  double order, int give_log)
 {
-    double u, tmp1, tmp2;
+    double u, tmp;
 
     if (!R_FINITE(shape1) ||
-	!R_FINITE(scale) ||
 	!R_FINITE(shape2) ||
-	!R_FINITE(limit) ||
-	!R_FINITE(order) ||
+	!R_FINITE(scale)  ||
+	!R_FINITE(order)  ||
 	shape1 <= 0.0 ||
-	scale <= 0.0 ||
 	shape2 <= 0.0 ||
-	limit <= 0.0 ||
+	scale  <= 0.0 ||
 	order <= -shape1 * shape2)
 	return R_NaN;
 
-    u = R_pow(limit, shape2);
-    tmp1 = R_pow(scale, shape2);
-    tmp2 = order / shape2;
+    if (limit <= 0.0)
+	return 0;
 
-    return R_pow(scale, order) * gammafn(shape1 + tmp2) * pgamma(u, shape1 + tmp2, tmp1, 1, 0) / gammafn(shape1) + R_pow(limit, order) * pgamma(u, shape1, tmp1, 0, 0);
+    tmp = shape1 + order / shape2;
+
+    u = exp(shape2 * (log(limit) - log(scale)));
+
+    return R_pow(scale, order) * gammafn(tmp)
+	* pgamma(u, tmp, 1.0, 1, 0) / gammafn(shape1)
+	+ R_pow(limit, order) * pgamma(u, shape1, 1.0, 0, 0);
 }
