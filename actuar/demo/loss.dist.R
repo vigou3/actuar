776e8f343aa3209b94226df84a2315e6c92b3c17
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2006-10-18 01:41:02 +0000

    Édition du démo toujours en cours.

--- actuar/demo/loss.dist.R
+++ actuar/demo/loss.dist.R
@@ -11,6 +11,63 @@ opar <- par(ask = interactive() &&
             (.Device %in% c("X11", "GTK", "gnome", "windows","quartz"))
             mfrow = c(2, 2))
 
+### A utily function to create graphs for probability laws
+showgraphs <- function(fun, par, mfrow = c(2, 2))
+{
+    dist <- switch(fun,
+                   trbeta = "TRANSFORMED BETA DISTRIBUTION",
+                   burr = "BURR DISTRIBUTION",
+                   llogis = "LOGLOGISTIC DISTRIBUTION",
+                   paralogis = "PARALOGISTIC DISTRIBUTION",
+                   genpareto = "GENERALIZED PARETO DISTRIBUTION",
+                   pareto = "PARETO DISTRIBUTION",
+                   pareto1 = "SINGLE PARAMETER PARETO DISTRIBUTION",
+                   invburr = "INVERSE BURR DISTRIBUTION",
+                   invpareto = "INVERSE PARETO DISTRIBUTION",
+                   invparalogis = "INVERSE PARALOGISTIC DISTRIBUTION",
+                   trgamma = "TRANSFORMED GAMMA DISTRIBUTION",
+                   invtrgamma = "INVERSE TRANSFORMED GAMMA DISTRIBUTION",
+                   invgamma = "INVERSE GAMMA DISTRIBUTION",
+                   invweibull = "INVERSE WEIBULL DISTRIBUTION",
+                   invexp = "INVERSE EXPONENTIAL DISTRIBUTION",
+                   lgamma = "LOGGAMMA DISTRIBUTION",
+                   lnorm= "LOGNORMAL DISTRIBUTION",
+                   gamma = "GAMMA DISTRIBUTION",
+                   exp = "EXPONENTIAL DISTRIBUTION",
+                   weibull = "WEIBULL DISTRIBUTION")
+
+    df   <- match.fun(paste("d", fun, sep = ""))
+    pf   <- match.fun(paste("p", fun, sep = ""))
+    rf   <- match.fun(paste("r", fun, sep = ""))
+    mf   <- match.fun(paste("m", fun, sep = ""))
+    levf <- match.fun(paste("lev", fun, sep = ""))
+
+    formals(df)[names(par)]   <- par
+    formals(pf)[names(par)]   <- par
+    formals(rf)[names(par)]   <- par
+    formals(mf)[names(par)]   <- par
+    formals(levf)[names(par)] <- par
+
+    x <- rf(1000)
+    limit <- seq(0, max(x), length = 10)
+    k <- seq(1, 5, length = 10)
+
+    op <- par(mfrow = mfrow, oma = c(0, 0, 2, 0))
+
+    hist(x, prob = TRUE, main = "Density")
+    curve(df(x), add = TRUE, col = "blue", lwd = 2, lty = 2)
+    plot(ecdf(x), pch = "", main = "Distribution function", lwd = 2)
+    curve(pf(x), add = TRUE, col = "blue", lwd = 2, lty = 2)
+    plot(k, emm(x, k), type = "l", lwd = 2, main = "Raw moments")
+    lines(k, mf(k), col = "blue", lwd = 2, lty = 2)
+    plot(limit, elev(x)(limit), type = "l", lwd = 2,
+         main = "Limited expected value")
+    lines(limit, levf(limit), col = "blue", lwd = 2, lty = 2)
+    title(main = dist, outer = TRUE)
+
+    par(op)
+}
+
 
 ###
 ### PROBABILITY LAWS
@@ -29,50 +86,90 @@ opar <- par(ask = interactive() &&
 
 ## TRANSFORMED BETA FAMILY
 
-## Tranformed beta distribution
-k <- 1:10
-shape1 <- 3
-shape2 <- 4
-shape3 <- 5
-rate <- 10
-
-showgraphs <- function(fun, par)
-{
-    df   <- match.fun(paste("d", fun, sep = ""))
-    pf   <- match.fun(paste("p", fun, sep = ""))
-    rf   <- match.fun(paste("r", fun, sep = ""))
-    mf   <- match.fun(paste("m", fun, sep = ""))
-    levf <- match.fun(paste("lev", fun, sep = ""))
-
-    formals(df)[names(par)]   <- par
-    formals(pf)[names(par)]   <- par
-    formals(rf)[names(par)]   <- par
-    formals(mf)[names(par)]   <- par
-    formals(levf)[names(par)] <- par
-
-    x <- rf(1000)
-    limit <- seq(0, max(x), length = 10)
-    k <- 1:10
-
-    op <- par(mfrow = c(2, 2))
-
-    hist(x, prob = TRUE, main = "Density")
-    curve(df(x), add = TRUE, col = "blue", lwd = 2, lty = 2)
-    plot(ecdf(x), pch = "", main = "Distribution function", lwd = 2)
-    curve(pf(x), add = TRUE, col = "blue", lwd = 2, lty = 2)
-    plot(k, emm(x, k), type = "l", lwd = 2, main = "Raw moments")
-    lines(k, mf(k), col = "blue", lwd = 2, lty = 2)
-    plot(limit, elev(x)(limit), type = "l", lwd = 2,
-         main = "Limited expected value")
-    lines(limit, levf(limit), col = "blue", lwd = 2, lty = 2)
-
-    par(op)
-}
-
+## Transformed beta distribution
 showgraphs("trbeta", list(shape1 = 3, shape2 = 4, shape3 = 5, scale = 10))
 
+## Generalized Pareto distribution
+showgraphs("genpareto", list(shape1 = 3, shape2 = 4, scale = 10))
+
+## Burr distribution
+showgraphs("burr", list(shape1 = 3, shape2 = 4, scale = 10))
+
+## Inverse Burr distribution
+showgraphs("invburr", list(shape1 = 3, shape2 = 4, scale = 10))
+
+## Pareto distribution
+showgraphs("pareto", list(shape = 3, scale = 10))
+
+## Inverse Pareto distribution
+showgraphs("invpareto", list(shape = 0.01, scale = 10))
+
+## Paralogistic distribution
+showgraphs("paralogis", list(shape = 3, scale = 10))
+
+## Inverse paralogistic distribution
+showgraphs("invparalogis", list(shape = 3, scale = 10))
+
+## Loglogistic distribution
+showgraphs("llogis", list(shape = 3, scale = 10))
 
 
+## TRANSFORMED GAMMA FAMILY
+
+## Transformed gamma distribution
+showgraphs("trgamma", list(shape1 = 3, shape2 = 4, scale = 10))
+
+## Gamma distribution ('mgamma' and 'levgamma')
+showgraphs("gamma", list(shape = 3, scale = 10))
+
+## Weibull distribution ('mweibull' and 'levweibull')
+showgraphs("weibull", list(shape = 3, scale = 10))
+
+## Exponential distribution ('mexp' and 'levexp')
+showgraphs("exp", list(rate = 0.1))
+
+
+## INVERSE TRANSFORMED GAMMA FAMILY
+
+## Inverse transformed gamma distribution
+showgraphs("invtrgamma", list(shape1 = 3, shape2 = 4, scale = 10))
+
+## Inverse gamma distribution
+showgraphs("invgamma", list(shape = 3, scale = 10))
+
+## Inverse Weibull distribution
+showgraphs("invweibull", list(shape = 3, scale = 10))
+
+## Inverse exponential distribution
+showgraphs("invexp", list(rate = 0.1))
+
+
+## OTHER DISTRIBUTIONS
+
+## Lognormal distribution ('mlnorm' and 'levlnorm')
+showgraphs("lnorm", list(meanlog = 5, sdlog = 1))
+
+## Single parameter Pareto distribution
+showgraphs("pareto1", list(shape = 3, min = 10))
+
+
+###
+### GROUPED DATA MANIPULATION
+###
+
+## Creation of grouped data objects
+x <- grouped.data(group = c(0, 25, 50, 100, 150, 250, 500),
+                  line1 = c(30, 31, 57, 42, 65, 84),
+                  line2 = c(26, 33, 31, 19, 16, 11))
+
+## Extraction and replacement: only "[" and "[<-" are officially
+## supported
+x[, 1]                                  # group boundaries
+x[1]                                    # notice the difference
+x[, -1]                                 # group frequencies
+x[1:3,]                                 # first 3 groups
+x[1, 2] <- 22; x                        # frequency replacement
+x[1, 1] <- c(0, 20); x                  # boundary replacement
 
 
 par(op)
