c9eecbf08104f242bac058bae09ea6d3d734bdba
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2007-02-08 05:08:46 +0000

    Mise a jour de la copie de travail.

--- actuar/R/simpf2.R
+++ actuar/R/simpf2.R
@@ -11,28 +11,56 @@ simpf2 <- function(nodes, model, weights)
     ## there is only the portfolio.
     n.above <- 1
 
-    #for (i in seq_along(nodes))
-    for (i in 1)
+    for (i in seq_along(nodes))
     {
+        ## Number of nodes at the current level, recycled if
+        ## necessary
         n.current <- rep(nodes[[i]], length = n.above)
-        Call <- substitute(model[[i]])
+
+        ## Extract simulation model for the level
+        Call <- model[[i]]
+
+        ## Correctly repeat the mixing parameters of all levels above
+        ## the current one. Normally, only the immediately above
+        ## mixing parameter will be used in the model for a level, but
+        ## the code here allows for other preceeding parameters to be
+        ## used.
+        for (param in intersect(all.vars(Call), nnodes))
+        {
+            eval(substitute(x <- rep(x, n.current),
+                            list(x = as.name(param))))
+            cat(param, "\n", eval(as.name(param)), fill = TRUE)
+        }
+
+        ## Add the number of variates to the model.
+        Call$n <- sum(n.current)
         print(Call)
-        Call$n <- n.current
+
+        ## Simulation
         assign(nnodes[[i]], eval(Call))
-        n.above <- n.current
+        cat(eval(as.name(nnodes[[i]])), "\n\n", fill = TRUE)
+
+        ## Remember the total number of nodes at the current level for
+        ## the next iteration.
+        n.above <- sum(n.current)
     }
+
+    list(group, contract, year)
 
 }
 
-nodes <- list(class = 3,
+nodes <- list(group = 3,
               contract = c(3, 4, 2),
               year = c(5, 4, 3, 4, 3, 5, 4, 4, 5))
-model <- list(class = expression(rgamma(2, 3)),
-              contract = expression(rgamma(class, 4)),
-              year = expression(rpois(contract)))
 
-model <-
+model <- expression(group = rgamma(2, 3),
+    contract = rgamma(group, 4),
+    year = rpois(contract))
 
-simpf2(nodes = nodes, model = list(class = rgamma(2, 3),
-              contract = rgamma(class, 4),
-              year = rpois(contract)))
+simpf2(nodes = nodes, model = model)
+
+model <- expression(group = rgamma(2, 3),
+    contract = rgamma(group, 4),
+    year = rpois(contract * group))
+
+simpf2(nodes = nodes, model = model)
