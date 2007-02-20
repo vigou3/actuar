f8d9aa23fc3d85d349007bef773b6fe057e0ddb5
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2007-02-20 16:03:19 +0000

    Many small but useful changes to aggregate.simpf():
    1. now possible to make summaries by "year" only;
    2. level names in 'by' are partially matched (thus 'by' is now numeric
    in the code);
    3. for across "year" summaries, the summary column name is now the
    name of the function (e.g. "sum").
    
    Also: some editing of the comments and shuffling around of code snippets.

--- actuar/R/aggregate.simpf.R
+++ actuar/R/aggregate.simpf.R
@@ -8,52 +8,55 @@
 aggregate.simpf <- function(x, by = names(x$nodes), FUN = sum, ...)
 {
     level.names <- names(x$nodes)       # level names
+    nlevels <- length(level.names)      # number of levels
+
+    by <- pmatch(by, level.names, duplicates.ok = TRUE)
+
+    ## Version of FUN able to work on lists
+    fun <- function(x, ...) FUN(unlist(x), ...)
 
     ## The most common case should be to aggregate claim amounts by
-    ## node. This case is very simple, so we treat it separately.
-    if (identical(by, level.names))
+    ## node. This case being very simple, it is treated separately.
+    if (identical(length(by), nlevels))
         return(cbind(x$classification,
                      array(sapply(x$data, FUN, ...), dim(x$data),
                            dimnames = dimnames(x$data))))
+
+    ## Summaries only by last level (years) are also simple to handle.
+    if (identical(by, nlevels))
+        return(apply(x$data, 2, fun, ...))
 
     ## The other possibilities require to split the data in groups as
     ## specified in argument 'by'. If the last level (years) is in
     ## 'by', then the matrix structure must be retained to make the
     ## summaries. Otherwise, it can just be dropped since summaries
     ## will span the years of observation.
-    years <- tail(level.names, 1)       # name of last level (years)
-
-    ## Summaries only by last level (years) do not make sense.
-    if (identical(by, years))
-        stop(paste("'by' must contain at least one level other than '",
-                   years, "'", sep = ""))
+    years <- level.names[nlevels]       # name of last level (years)
 
     ## Convert the sequence of subscripts into factors by pasting the
     ## digits together.
-    rows <- setdiff(by, years)          # groups other than years
+    rows <- setdiff(by, nlevels)        # groups other than years
     s <- x$classification[, rows, drop = FALSE] # subscripts
     f <- apply(s, 1, paste, collapse = "")      # factors
+    s <- s[match(unique(f), f),]                # unique subscripts
     xx <- split(x$data, f)                      # split data
 
-    ## Function FUN will have to work on lists from now on.
-    fun <- function(x, ...) FUN(unlist(x), ...)
-
     ## Make summaries
-    if (years %in% by)
+    if (nlevels %in% by)
     {
         xx <- lapply(xx, matrix, ncol = ncol(x$data))
-        res <- t(sapply(xx, function(x) apply(x, 2, fun, ...)))
+        res <- t(sapply(xx, function(x, ...) apply(x, 2, fun, ...), ...))
         cols <- colnames(x$data)
     }
     else
     {
         res <- sapply(xx, fun, ...)
-        cols <- ""
+        cols <- deparse(substitute(FUN))
     }
 
     ## Return results as a matrix
-    structure(cbind(s[match(unique(f), f),], res),
-              dimnames = list(NULL, c(rows, cols)))
+    structure(cbind(s, res),
+              dimnames = list(NULL, c(level.names[rows], cols)))
 }
 
 frequency.simpf <- function(x, by = names(x$nodes))
