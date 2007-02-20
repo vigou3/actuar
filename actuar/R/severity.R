0351a79bbf0147a3e15c2689dd98ab50c994e68c
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2007-02-20 03:53:48 +0000

    'severity' functions completed.

--- actuar/R/severity.R
+++ actuar/R/severity.R
@@ -9,11 +9,12 @@
 ### AUTHORS: Louis-Philippe Pouliot,
 ### Vincent Goulet <vincent.goulet@act.ulaval.ca>
 
-### Create a new generic
+### New generic
 severity <- function(x, ...) UseMethod("severity")
 
-### Default method new matrix is created where all the data is
-### displayed and empty spaces are filled with NA.
+### Default method for vectors and matrices of vectors (that is, lists
+### withour 'dim' attribute or with a 'dim' attribute of length
+### 2)
 severity.default <- function(x, bycol = FALSE, ...)
 {
     if (length(dim(x)) > 2)
@@ -28,7 +29,7 @@ severity.default <- function(x, bycol = FALSE, ...)
     if (bycol)
     {
         lengths <- colSums(frequencies, na.rm = TRUE)
-        mat <- matrix(NA, max(lengths), ncol(x))
+        mat <- matrix(NA, max(lengths), ncol(x), dimnames = dimnames(x))
         for (i in seq_len(ncol(x)))
             if (0 < (lengthi <- lengths[i]))
                 mat[seq_len(lengthi), i] <- unlist(x[!is.na(x[, i]), i])
@@ -36,7 +37,8 @@ severity.default <- function(x, bycol = FALSE, ...)
     else
     {
         lengths <- rowSums(frequencies, na.rm = TRUE)
-        mat <- matrix(NA, nrow(x), max(lengths))
+        mat <- matrix(NA, nrow(x), max(lengths),
+                      dimnames = list(rownames(x), NULL))
         for (i in seq_len(nrow(x)))
             if (0 < (lengthi <- lengths[i]))
                 mat[i, seq_len(lengthi)] <- unlist(x[i, !is.na(x[i, ])])
@@ -44,65 +46,94 @@ severity.default <- function(x, bycol = FALSE, ...)
     drop(mat)
 }
 
-## 'simpf' objects are treated more specifically, being displayed more
-## appropriately with respect to the actuarial context. The
-## possibility of treating separetly the last years of experience is
-## also given.
-
-severity.simpf <- function(x, by = head(x$node, -1), split = NULL, ...)
+## Method for 'simpf' objects. Two differences with the default
+## method: 1) data can be grouped according to the levels of the
+## portfolio; 2) data can be split by column (e.g. data from all years
+## but the last and data from the last year).
+severity.simpf <- function(x, by = head(names(x$node), -1),
+                           splitcol = NULL, ...)
 {
-    if (is.numeric(split))
-        if (any(split < 0 | split > ncol(x$data)))
-            stop("invalid split column specification")
-    if (is.character(split))
-        if (any(is.na(split <- match(split, colnames(x$data)))))
-            stop("invalid split column specification")
-
     level.names <- names(x$nodes)       # level names
+    ci <- seq_len(ncol(x$data))         # column indexes
 
+    ## Sanity checks
     if (identical(by, level.names))
     {
         warning("nothing to do")
         return(x)
     }
+    if (any(!by %in% level.names))
+        stop("invalid 'by' specification")
 
+    ## Convert character 'splitcol' to numeric and then from numeric
+    ## or NULL to boolean.
+    if (is.character(splitcol))
+        splitcol <- pmatch(splitcol, colnames(x$data), duplicates.ok = TRUE)
+    if (is.numeric(splitcol) || is.null(splitcol))
+        splitcol <- ci %in% splitcol
+
+    ## Unroll claim amounts by column; simplest case
     if (tail(level.names, 1) %in% by)
     {
         if (length(by) > 1)
-            stop(paste("'", tail(level.names, 1), "' cannot be combined with another level"))
-        res <- NextMethod(object = x$data, bycol = TRUE)
+            stop("invalid 'by' specification")
+        x <- x$data
+        res <- NextMethod(bycol = TRUE)
+        return(list(first = res[, !splitcol],
+                    last = if (all(!splitcol)) NULL else res[, splitcol]))
     }
+
+    ## Unrolling per row (or group of rows) is more work. It requires
+    ## to split the columns of the matrix first, and then to apply the
+    ## unrolling procedure twice (if 'splitcol' != NULL).
+
+    ## Utility function
+    fun <- function(x) unlist(x[!is.na(x)])
+
+    ## Split rows according to the 'by' argument.
+    s <- x$classification[, by, drop = FALSE]   # subscripts
+    f <- apply(s, 1, paste, collapse = "")      # factors
+    s <- s[match(unique(f), f),]                # unique subscripts
+
+    ## Keep the 'splitcol' columns for later use.
+    x.last <- x$data[, splitcol]
+
+    ## Unroll the "main" block of columns.
+    if (all(splitcol))
+        res.first <- NULL
     else
     {
-        s <- x$classification[, by, drop = FALSE]   # subscripts
-        f <- apply(s, 1, paste, collapse = "")      # factors
-        xx <- cbind(lapply(split(x$data, f), unlist)) # split data
-        res <- NextMethod(object = xx)
+        x <- cbind(lapply(split(x$data[, !splitcol], f), fun))
+        res.first <- NextMethod(bycol = FALSE)
+        res.first <-
+            if (0 < (nc <- ncol(res.first)))
+            {
+                dimnames(res.first) <-
+                    list(NULL, paste("claim", seq_len(nc), sep = "."))
+                cbind(s, res.first)
+            }
+            else
+                NULL
     }
-    res
-}
 
-    x <- x$data
-    y <- x
-    if (y.exclude %% 1 != 0 | y.exclude < 0)
-        stop("y.exclude must be a positive integer")
-    if (!bycol) names.first <- list(unlist(dimnames(x)[1]), NULL)
-    else names.first <- list(NULL, unlist(dimnames(x)[2])[1:(ncol(x) - y.exclude)])
-    x <- x[ , seq(length = ncol(x) - y.exclude)]
-    res <- NextMethod()
-    dimnames(res) <- names.first
-
-    if (y.exclude > 0)
+    ## Unroll the 'splitcol' block of columns.
+    if (all(!splitcol))
+        res.last <- NULL
+    else
     {
-        if (!bycol) names.last <- names.first
-        else names.last <- list(NULL, unlist(dimnames(y)[2])[(ncol(y) - y.exclude + 1):(ncol(y))])
-
-        x <- y[ , seq(ncol(y) - y.exclude + 1, ncol(y))]
-        if (!is.matrix(x)) x <- as.matrix(x)
-        mat.last <- NextMethod()
-        dimnames(mat.last) <- names.last
-        res <- list(mat.first = res, mat.last = mat.last)
+        x <- cbind(lapply(split(x.last, f), fun))     # split data
+        res.last <- NextMethod(bycol = FALSE)
+        res.last <-
+            if (0 < (nc <- ncol(res.last)))/
+            {
+                dimnames(res.last) <-
+                    list(NULL, paste("claim", seq_len(nc), sep = "."))
+                cbind(s, res.last)
+            }
+            else
+                NULL
     }
-    print(res)
 
+    ## Return the result as a list.
+    list(first = res.first, last = res.last)
 }
