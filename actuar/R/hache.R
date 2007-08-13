796f5f1684c3202b0c49b9f242d28c425d8aee59
Tommy Ouellet <tommy.ouellet.1@ulaval.ca>
2007-08-13 15:38:53 +0000

    Hachemeister model. Still a few things to correct.

---
+++ actuar/R/hache.R
@@ -0,0 +1,110 @@
+### ===== actuar: an R package for Actuarial Science =====
+###
+### Credibility in the Regression Case
+###
+### Regression models...
+###
+### AUTHORS: Tommy Ouellet, Vincent Goulet <vincent.goulet@act.ulaval.ca>
+
+hache <- function(X, Y, weights, TOL = 1E-6, echo = FALSE, ...)
+{
+    Call <- match.call()
+
+    ## X = data matrix (I x T)
+    ## Y = design matrix (T x N)
+    I <- nrow(X)     # number of contracts
+    T <- ncol(X)     # number of years
+    N <- ncol(Y)     # dimension of design matrix
+    
+    ## Coefficients for each contract
+    beta <- matrix(0, nrow = I, ncol = N)
+    for (k in 1:I) beta[k, ] = coef(lm(X[k, ] ~ Y[, -1], weights = weights[k, ]))
+    
+    ## Estimation of s^2.
+    s2 <- 0
+    for (k in 1:I)
+    {
+        diff <- X[k, ] - Y %*% beta[k, ]
+        s2 <- s2 + t(diff) %*% solve(diag(weights[k, ])) %*% diff
+    }
+    s2 = as.vector( s2 / (I * (T - N)) )
+
+    ## Diagonal matrices filled with diagonal elements one are used
+    ## as starting values for the Zi matrices. The matrix of each
+    ## contract being of length N x N, the resulting matrices are
+    ## stocked in an array of length N x N x I where I is the
+    ## number of contracts.
+    Z <- array(as.vector(diag(N)), dim = c(N, N, I))
+    
+    ## First estimation of betaTotal.
+    t1 = t2 = 0
+    for (k in 1:I)
+    {
+        t1 = t1 + Z[, , k]
+        t2 = t2 + Z[, , k] %*% beta[k, ]
+    }
+    betaTotal <- solve(t1) %*% t2
+    
+    ## Iterative estimation of the parameters.
+    repeat
+    {
+        ## Transferring the value of betaTotal in another variable.
+        ## This value will be used as the stop-criterion.
+        if (echo) print(as.vector(betaTotal))
+        betaTotal2 <- betaTotal 
+
+        ## Estimation of A.
+        A <- 0
+        for (k in 1:I)
+        {
+            diff <- beta[k, ] - betaTotal
+            A <- A + Z[, , k] %*% diff %*% t(diff)
+        }
+        A <- A / (I - 1)
+
+        ## Estimation of the Zi matrices.
+        for (k in 1:I) Z[, , k] <- A %*% solve( ( A + s2 * solve( t(Y) %*% solve(diag(weights[k, ])) %*% Y ) ) )
+
+        ## New estimation of betaTotal.
+        t1 = t2 = 0
+        for (k in 1:I)
+        {
+            t1 = t1 + Z[, , k]
+            t2 = t2 + Z[, , k] %*% beta[k, ]
+        }
+        betaTotal <- solve(t1) %*% t2
+        
+        ## Stop-criterion.
+        if (max(abs( (betaTotal - betaTotal2) / betaTotal2 ) ) < TOL) break
+    }
+
+    ## Final estimation of A and Z with the final EspB.
+    A <- 0
+    for (k in 1:I)
+    {
+        diff <- beta[k, ] - betaTotal
+        A <- A + Z[, , k] %*% diff %*% t(diff)
+    }
+    A <- A / (I - 1)
+
+    for (k in 1:I) Z[, , k] <- A %*% solve( ( A + s2 * solve( t(Y) %*% solve(diag(weights[k, ])) %*% Y ) ) )
+
+    ## Credibility-adjusted estimator for beta.
+    betaAdj <- matrix(0, nrow = I, ncol = N)
+    for (k in 1:I) betaAdj[k, ] = Z[, , k] %*% beta[k, ] + (diag(N) - Z[, , k]) %*% betaTotal
+
+    ## Credibility premiums
+    p <- matrix(0, nrow = I, ncol = T)
+    for (k in 1:I) p[k, ] = t( Y %*% betaAdj[k, ] )
+
+    ## Results
+    structure(list(beta = beta,
+                   betaAdj = betaAdj,
+                   betaTotal = betaTotal,
+                   cred = Z,
+                   s2 = s2,
+                   a = A,
+                   p = p,
+                   call = Call),
+              class = "hache")
+}
