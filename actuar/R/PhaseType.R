cde9ba7192deb2233247a8543f14ad68d7e0ce33
Christophe Dutang <christophe.dutang@univ-lemans.fr>
2007-10-05 19:44:19 +0000

    bug fix in {q,m,mgf}phasetype. but problems for the last 2 functions still persist resulting from wrong implementation or wrong formula?

--- actuar/R/PhaseType.R
+++ actuar/R/PhaseType.R
@@ -46,12 +46,22 @@ pphasetype <- function(q, pi, T, m, lower.tail = TRUE, log.p = FALSE)
     
     onesM <- rep(1,m)
 
-    survivalProb <- .Call("calcMatExpGen", x, pi, T, onesM)
+    survivalProb <- .Call("calcMatExpGen", q, pi, T, onesM)
 
-    if(!log)
-        return(1 - survivalProb)
+    if(!log.p)
+    { 
+        if(lower.tail)
+            return(1 - survivalProb)
+        else
+            return(survivalProb)
+    }
     else
-        return( log(1 - survivalProb) )
+    {
+        if(lower.tail)
+            return( log(1 - survivalProb) )
+        else
+            return( log(survivalProb) )
+    }
 }
 
 qphasetype <- function(p, pi, T, m, lower.tail = TRUE, log.p = FALSE)
@@ -59,7 +69,19 @@ qphasetype <- function(p, pi, T, m, lower.tail = TRUE, log.p = FALSE)
 #???
     
 rphasetype <- function(n, pi, T, m)
-    .Call("randphasetype", n, pi, T, -T %*% rep(1,m) )
+{
+    if(length( dim(T) ) != 2)
+    {
+        stop("T is not a 'mxm' matrix")
+    }
+    if(dim(T)[1] != dim(T)[2])
+    {
+        stop("T is not a 'mxm' matrix")
+    }    
+    
+    tzero <- c(-T %*% rep(1,m))    
+    .Call("randphasetype", n, pi, T, tzero )
+}    
 
 
 mphasetype <- function(order, pi, T, m)
@@ -83,18 +105,26 @@ mphasetype <- function(order, pi, T, m)
         if(as.integer(order) == order)
         {
             TpowN <- T
-            n <- order-1
+            
             factN <- 1
-            for(k in 1:n)
-            {
-                factN <- factN * k
-                TpowN <- TpowN %*% TpowN
+            if(order > 1)
+            { #prod(T==diag(diag(T)))
+                for(k in 2:order)
+                {
+                    
+                    
+                    factN <- factN * k
+                    TpowN <- TpowN %*% T
+                    cat("-",k)
+                    print(TpowN)
+                }
+                cat("\nn!",factN,"\n")
             }
             return( factN * (-1)^order * pi %*% solve(TpowN, diag(m)) %*% onesM )
         }
         else
         {
-            stop("non integer order is not supported")
+            stop("non integer orders are not supported")
         }
     }
     
@@ -103,23 +133,36 @@ mphasetype <- function(order, pi, T, m)
 mgfphasetype <- function(x, pi, T, m, log = FALSE)
 {
      if(length( dim(T) ) != 2)
-    {
-        stop("T is not a 'mxm' matrix")
-    }
-    if(dim(T)[1] != dim(T)[2])
-    {
-        stop("T is not a 'mxm' matrix")
-    }    
-    
-    onesM <- rep(1,m)
-    tzero <- c(-T %*% onesM)
+     {
+         stop("T is not a 'mxm' matrix")
+     }
+     if(dim(T)[1] != dim(T)[2])
+     {
+         stop("T is not a 'mxm' matrix")
+     }    
      
-    temp <- solve(-x*diag(m) - T , diag(m) )
+     onesM <- rep(1,m)
+     tzero <- c(-T %*% onesM)
 
+     routine <- function(x)
+     {
+         options(show.error.messages = FALSE)
+         #test the matrix inversion
+         #if failed, testDiag is an invisible object of class 'try-error'
+         testDiag <- try( solve(-x*diag(m) - T , diag(m) ) )
+         options(show.error.messages = TRUE) #revert to default
+            
+         if(class(testDiag) == "try-error")          
+             return( NaN )
+         else             
+             return( pi %*% solve(-x*diag(m) - T , diag(m) ) %*% tzero )
+     }
+     
+     
      if(!log)
-         return( pi %*% temp %*% tzero )
+         return( sapply(x, routine) )
      else
-         return( log( pi %*% temp %*% tzero ) )
+         return( log( sapply(x, routine) ) )
 }
     
 
