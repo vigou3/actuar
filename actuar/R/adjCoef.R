99622f8ca71e9039b4896db57d3726e919ee5198
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2007-10-16 06:13:30 +0000

    Function adjCoef() completely revised.

--- actuar/R/adjCoef.R
+++ actuar/R/adjCoef.R
@@ -1,174 +1,195 @@
 ### ===== actuar: an R package for Actuarial Science =====
 ###
-### Ruin Theory
+### Compute the adjustment coefficient in ruin theory, that is the
+### smallest (strictly) positive root of the Lundberg equation
 ###
-### Compute the adjustment coefficient R, the (strictly) positive
-### root of the Lundberg equation :
-###         h(r) = E[ e^(r X - r c W) ] = 1
-### where X is the generic claim size random variable,
-### W the inter-occurence time, and c the premium rate.
-### If c does not respect the postive safety loading constraint
-### E[X-cW]<0, R does not exist, the function returns 0,
-### otherwise, it returns R>0.
+###      h(r) = E[e^(r X - r c W)] = 1,
+###
+### where X is the claim size random variable, W the inter-occurence
+### time and c the premium rate.
 ###
 ### AUTHORS: Christophe Dutang, Vincent Goulet <vincent.goulet@act.ulaval.ca>
-###
 
-adjCoef <- function(mgf.claim, mgf.wait, premium, upper, h, reinsurance=c("none","proportional","excess-of-loss"), from, to, n=101, toplot=FALSE)
+adjCoef <- function(mgf.claim, mgf.wait = mgfexp(x), premium, upper.bound, h,
+                    reinsurance = c("none", "proportional", "excess-of-loss"),
+                    from, to, n = 101)
 {
     reinsurance <- match.arg(reinsurance)
-    
-    ## check arguments
-    if(missing(mgf.claim) && missing(h))
-        stop("missing arguments: 'mgf.claim' or 'h' is needed")
-    
-    if(missing(upper))
-        stop("missing argument 'upper', the upper bound of the m.g.f. of claim size distribution")
 
-    if(!is.numeric(upper))
-        stop("wrong argument 'upper', which must be a 'numeric'")
+    ## Sanity check
+    if (missing(mgf.claim) && missing(h))
+        stop("one of 'mgf.claim' or 'h' is needed")
 
-    if(!missing(h))
+    ## === NO REINSURANCE CASE ===
+    ##
+    ## Moment generating functions are unidimensional, premium rate
+    ## and adjustment coefficient are both single numeric values.
+    if (reinsurance == "none")
     {
-        if(!is.function(h) || is.null(h))
-            stop("wrong argument 'h', which must be a function")
-    }
-    
-    if(!missing(mgf.claim))
-    {
-        if(!is.function(mgf.claim))
-            stop("wrong argument 'mgf.claim', which must be a function")        
-    
-        if(!missing(mgf.wait))
+        ## For each of 'mgf.claim', 'mgf.wait' and 'h' (if needed): if
+        ## the expression is only the name of a function (say f),
+        ## build a call 'f(x)'. Otherwise, check that the expression
+        ## is a function call containing an 'x'. Taken from 'curve'
+        ## and 'discretize'.
+        ##
+        ## NOTE: argument 'h' will be used iff 'mgf.claim' is missing,
+        ## thereby giving priority to 'mgf.claim'.
+        if (missing(mgf.claim))
         {
-            if(!is.function(mgf.wait))
-                stop("wrong argument 'mgf.wait', which must be a function")
+            sh <- substitute(h)
+            if (is.name(sh))
+            {
+                fcall <- paste(sh, "(x)")
+                h <- parse(text = fcall)
+            }
+            else
+            {
+                if (!(is.call(sh) && match("x", all.vars(sh), nomatch = 0)))
+                    stop("'h' must be a function or an expression containing 'x'")
+                h <- sh
+            }
         }
         else
-            stop("missing argument 'mgf.wait'")
-                
-        if(missing(premium))
-            stop("missing argument 'premium'")
-    }  
-
-    #reinsurance
-    if(reinsurance != "none")
-    {
-        if(missing(from) || missing(to))
-            stop("missing argument 'from' and/or 'to'")
-        
-        if(!is.numeric(from) || !is.numeric(to))
-            stop("wrong argument 'from' and/or 'to', which must be a numeric")
-        
-        if(!is.function(premium) )
-            stop("wrong argument 'premium', which must be a function when using reinsurance")
-
-        if(!is.character(reinsurance) || ( reinsurance != "proportional" && reinsurance != "excess-of-loss") )
-            stop("wrong argument 'reinsurance'")
-    }
-
-    #no reinsurance
-    if(reinsurance == "none" && !is.numeric(premium))
-        stop("wrong argument 'premium', which must be a numeric")
-
-    
-    ## compute the adjustment coefficient
-
-    #non reinsurance
-    if(reinsurance == "none")
-    {      
-        
-        if(!missing(mgf.claim)) h <- function(r) mgf.claim(r)*mgf.wait(-r*premium)
- 
-        
-    #compute the adjustment coefficient the unique positive root of h(r)=1
-    #with h(r) = M_X(r) * M_W(-r*premRate) in the case of independence
-    #where M_? stands for the moment generating function
-        
-        eqLundberg <-function(r) ( h(r) - 1 )^2
-
-        interval <- c( 0, upper-.Machine$double.eps )
-
-    #minimisation through 'optimize'
-    # le 'return' est obligatoire car sinon R continue à exéctuer le code
-    # du coup, il renvoie NULL
-        return(optimize( eqLundberg, interval, tol = sqrt(.Machine$double.eps))$minimum)
-    }
-
-  
-    #reinsurance
-    if(reinsurance != "none")
-    {
-        retentionVect <- seq(from, to, length.out = n)
-        
-        #if the mgf.{claim and wait} is defined
-        if(!missing(mgf.claim))
-            h <- function(r,ret) mgf.claim(r,ret)*mgf.wait(-r*premium(ret))
-        #otherwise 'h' is defined by the user
-        
-        # function ret -> R(ret) 
-        adjustCoefFunction <- function(ret)
         {
-            eqLundberg <-function(r) ( h(r,ret) -1 )^2           
-        
-            interval <- c( 0, upper-.Machine$double.eps )
-
-            #minimisation through 'optimize'
-            optimize( eqLundberg, interval, tol = sqrt(.Machine$double.eps))$minimum
+            smgfx <- substitute(mgf.claim)
+            if (is.name(smgfx))
+            {
+                fcall <- paste(smgfx, "(x)")
+                mgfx <- parse(text = fcall)
+            }
+            else
+            {
+                if (!(is.call(smgfx) && match("x", all.vars(smgfx), nomatch = 0)))
+                    stop("'mgf.claim' must be a function or an expression containing 'x'")
+                mgfx <- smgfx
+            }
+            smgfw <- substitute(mgf.wait)
+            if (is.name(smgfw))
+            {
+                fcall <- paste(smgfw, "(x)")
+                mgfw <- parse(text = fcall)
+            }
+            else
+            {
+                if (!(is.call(smgfw) && match("x", all.vars(smgfw), nomatch = 0)))
+                    stop("'mgf.wait' must be a function or an expression containing 'x'")
+                mgfw <- smgfw
+            }
+            h <- function(x) eval(mgfx) * eval(mgfw, list(x = -x * premium))
         }
-        
-        vectAdjustCoef <- sapply( retentionVect, adjustCoefFunction)
-        
-        fun <- approxfun(retentionVect, vectAdjustCoef, rule=2, method = "linear")
-        
-        comment(fun) <- reinsurance
-        class(fun) <- c("adjCoef", class(fun))
-        attr(fun, "x") <- retentionVect
-        attr(fun, "y") <- vectAdjustCoef
 
-        return( fun )
+        f <- function(r) (h(r) - 1)^2
+
+        return(optimize(f, c(0, upper.bound - .Machine$double.eps),
+                        tol = sqrt(.Machine$double.eps))$minimum)
     }
-    
+
+    ## === WITH REINSURANCE CASES ===
+    ##
+    ## Claim amount moment generating function is a function of 'x'
+    ## and the retention level 'y', inter-occurence time moment
+    ## generating function is a function of 'x', premium rate and
+    ## adjustment coefficient are both functions of the retention
+    ## level 'y'.
+    ##
+    ## Do same as in the no reinsurance case for each of 'mgf.claim',
+    ## 'mgf.wait' and 'h' (if needed) and also 'premium'. The first
+    ## must be functions of 'x' and 'y', whereas the last one is a
+    ## function of 'y' only.
+    if (missing(mgf.claim))
+    {
+        sh <- substitute(h)
+        if (is.name(sh))
+        {
+            fcall <- paste(sh, "(x, y)")
+            h <- parse(text = fcall)
+        }
+        else
+        {
+            if (!(is.call(sh) && all(match(c("x", "y"), all.vars(sh), nomatch = 0))))
+                stop("'h' must be a function or an expression containing 'x' and 'y'")
+            h <- sh
+        }
+    }
+    else
+    {
+        if (!is.function(premium))
+            stop("'premium' must be a function when using reinsurance")
+
+        smgfx <- substitute(mgf.claim)
+        if (is.name(smgfx))
+        {
+            fcall <- paste(smgfx, "(x, y)")
+            mgfx <- parse(text = fcall)
+        }
+        else
+        {
+            if (!(is.call(smgfx) && all(match(c("x", "y"), all.vars(smgfx), nomatch = 0))))
+                stop("'mgf.claim' must be a function or an expression containing 'x' and 'y'")
+            mgfx <- smgfx
+        }
+        smgfw <- substitute(mgf.wait)
+        if (is.name(smgfw))
+        {
+            fcall <- paste(smgfw, "(x)")
+            mgfw <- parse(text = fcall)
+        }
+        else
+        {
+            if (!(is.call(smgfw) && match("x", all.vars(smgfw), nomatch = 0)))
+                stop("'mgf.wait' must be a function or an expression containing 'x'")
+            mgfw <- smgfw
+        }
+        spremium <- substitute(premium)
+        if (is.name(spremium))
+        {
+            fcall <- paste(spremium, "(y)")
+            premium <- parse(text = fcall)
+        }
+        else
+        {
+            if (!(is.call(spremium) && match("y", all.vars(spremium), nomatch = 0)))
+                stop("'premium' must be a function or an expression containing 'y'")
+            premium <- spremium
+        }
+
+        h <- function(x, y) eval(mgfx) * eval(mgfw, list(x = -x * eval(premium)))
+    }
+
+    f <- function(x, y) (h(x, y) - 1)^2
+    retention <- seq(from, to, length.out = n)
+
+    ## Compute the adjustment coefficient for each retention level.
+    ## The output of 'sapply' is a matrix with minima in the first
+    ## line.
+    coef <- sapply(retention, optimize, f = f,
+                   interval = c(0, upper.bound-.Machine$double.eps),
+                   tol = sqrt(.Machine$double.eps))[1,]
+
+    ## Make a function from the (retention, coefficient) pairs
+    ## computed above, joining the points by straight line segments.
+    FUN <- approxfun(retention, coef, rule = 2, method = "linear")
+
+    comment(FUN) <- paste(toupper(substring(reinsurance, 1, 1)),
+                          substring(reinsurance, 2),
+                          " reinsurance",
+                          sep = "", collapse = "")
+    class(FUN) <- c("adjCoef", class(FUN))
+    attr(FUN, "call") <- sys.call()
+    FUN
 }
 
-plot.adjCoef <- function(x, type = "l", add = FALSE, ...)
+plot.adjCoef <- function(x, xlab = "x", ylab = "R(x)",
+                         main = "Adjustment Coefficient",
+                         sub = comment(x), type = "l", add = FALSE, ...)
 {
-    if("function" %in% class(x))
-    {
-        if(comment(x) == "proportional")
-        {
-            main <- "Adjustment Coefficient [Proportional reinsurance]"
-            xlab <- "a [retention rate]"
-            ylab <- "R(a)"
-        }
-        else
-        {
-            if(comment(x) != "excess-of-loss")
-                stop("internal error in adjCoef, wrong comment for an 'adjCoef' object")
-            
-            main <- "Adjustment Coefficient [Excess of loss reinsurance]"
-            xlab <- "L [retention limit]"
-            ylab <- "R(L)"
-        }
-
-        #Warning messages:
-        #1: "add" n'est pas un paramètre graphique in: plot.window(xlim, ylim, log, asp, ...) 
-        #2: "add" n'est pas un paramètre graphique in: plot.xy(xy, type, pch, lty, col, bg, cex, lwd, ...) 
-        #3: "add" n'est pas un paramètre graphique in: axis(side, at, as.graphicsAnnot(labels), tick, line, pos, outer,  
-        #4: "add" n'est pas un paramètre graphique in: axis(side, at, as.graphicsAnnot(labels), tick, line, pos, outer,  
-        #5: "add" n'est pas un paramètre graphique in: box(which = which, lty = lty, ...) 
-        #6: "add" n'est pas un paramètre graphique in: title(main, sub, xlab, ylab, line, outer, ...)
-                                                   
-        if(add) 
-            lines(attr(x, "x"), attr(x, "y"), main=main, xlab=xlab, ylab=ylab, type=type,...)
-        else
-            plot(attr(x, "x"), attr(x, "y"), main=main, xlab=xlab, ylab=ylab, type=type,...)
-    }
+    xx <- eval(expression(x), envir = environment(x))
+    yy <- eval(expression(y), envir = environment(x))
+    if (add)
+        lines(xx, yy, ..., main = main, xlab = xlab, ylab = ylab,
+              type = type)
+    else
+        plot(xx, yy, ..., main = main, xlab = xlab, ylab = ylab,
+             type = type)
+    mtext(sub, line = 0.5)
 }
-
-lines.adjCoef <- function(x, type = "l", ...)
-{
-    if("function" %in% class(x))
-        lines(attr(x, "x"), attr(x, "y"), type=type, add=TRUE, ...)    
-}
-
