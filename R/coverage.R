8ae41d99a5f64ef8ad0801f9782024ba816b1897
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2012-09-17 06:26:03 +0000

    Complete rewrite of coverage(). The previous version using do.call() no longer worked (without warnings) starting with R 2.15.1. Moreover, not using ifelse() improves speed of the output function drastically (~5 x).

--- R/coverage.R
+++ R/coverage.R
@@ -19,7 +19,8 @@ coverage <- function(pdf, cdf, deductible = 0, franchise = FALSE,
     ## there is a deductible or a limit and, of course, if the output
     ## function should compute the cdf.
     is.cdf <- missing(pdf) || is.null(pdf) # return cdf?
-    needs.cdf <- any(deductible > 0, limit < Inf, is.cdf) # cdf needed?
+    has.limit <- limit < Inf               # used often
+    needs.cdf <- any(deductible > 0, has.limit, is.cdf) # cdf needed?
 
     ## Sanity check of arguments
     if (any(deductible < 0, limit < 0, coinsurance < 0, inflation < 0))
@@ -31,10 +32,11 @@ coverage <- function(pdf, cdf, deductible = 0, franchise = FALSE,
     if (missing(cdf) & needs.cdf)
         stop("'cdf' must be supplied")
 
-    ## Quantites often used.
+    ## Quantites often used. Leave as expressions for the output
+    ## function to preserve accuracy.
     r <- 1 + inflation
-    d <- deductible/r
-    u <- limit/r
+    d <- if (inflation) substitute(d/r, list(d = deductible, r = r)) else deductible
+    u <- if (inflation) substitute(u/r, list(u = limit, r = r)) else limit
 
     ## The modified cdf or pdf are usually defined in branches. To
     ## avoid using nested ifelse(), we will rather rely on sets of
@@ -44,8 +46,7 @@ coverage <- function(pdf, cdf, deductible = 0, franchise = FALSE,
     ## The output function will have varying number of said
     ## expressions depending on the case that is dealt with. We will
     ## build the body of the output function piece by piece as we go
-    ## along. We start here with the first few lines that never
-    ## change.
+    ## along.
     e <- expression(Call <- match.call())
 
     ## One main discriminating factor is whether the cdf is needed for
@@ -63,99 +64,103 @@ coverage <- function(pdf, cdf, deductible = 0, franchise = FALSE,
         has.lower <- "lower.tail" %in% argn
         argn <- setdiff(argn, c("lower.tail", "log.p"))
 
-        ## If output function is a cdf:
-        ##
-        ## 1. Set its arguments to those of 'cdf'.
-        ## 2. Set the symbol representing the variable in function
-        ##    calls. Should be the first argument of the output
-        ##    function.
-        ## Initialization of the output function.
-        if (is.cdf)
+        ## Calculations in the output function are done by evaluating
+        ## function calls built upon the call to the output function
+        ## itself. This is convenient as we do not need to fiddle with
+        ## the values of the formal arguments.
+        if (is.cdf)                     # output function computes F(y)
         {
-            ## The output function will have the cdf formal arguments.
-            ## Object 'x' holds the symbol of the first argument.
+            ## The output function will have the same formal arguments
+            ## as the cdf. Object 'x' holds the symbol of the first
+            ## argument.
             argsFUN <- argv[argn]       # arguments of output function
             x <- as.name(argn[1])       # symbol
 
-            ## A function to compute F(y) is needed; one to compute
-            ## F(d) only if there is a deductible; one to also compute
-            ## 1 - F(d) for the per payment cases. Never need to
-            ## compute F(u).
+            ## Calls needed in this case:
+            ## 1. one to compute F(y);
+            ## 2. one to compute F(d) if there is a deductible;
+            ## 3. one to compute 1 - F(d) for the per payment cases.
+            ## Never need to compute F(u).
+            ##
+            ## If 'lower.tail' is available in 'cdf', then set it to
+            ## FALSE to compute S(d) = 1 - F(d) more accurately.
             e <- c(e,
-                   quote(F <- Call),
+                   quote(F <- Call),        # 1.
                    substitute(F[[1L]] <- as.name(fun),
                               list(fun = as.character(Call$cdf))))
             if (deductible)
             {
                 e <- c(e,
-                       Fd <- F,
+                       quote(Fd <- F),      # 2.
                        substitute(Fd[[2L]] <- a, list(a = d)))
                 if (!per.loss & has.lower)
                     e <- c(e,
-                           Sd <- Fd,
+                           quote(Sd <- Fd), # 3.
                            quote(Sd$lower.tail <- FALSE))
             }
         }
-        else
+        else                            # output function computes f(y)
         {
-            ## No function to compute F(y) needed. However, need to
-            ## compute F(d) for the per loss cases and 1 - F(d) for
-            ## the per payment cases; compute 1 - F(u) when there is a
-            ## limit.
-            if (deductible)
+            ## When there is a limit, we will need to compute 1 - F(u)
+            ## as is or using 'lower.tail = FALSE' to improve
+            ## accuracy. For clarity in the output function, we will
+            ## use Fu as object name in the first case and Su in the
+            ## second.
+            if (has.limit)
+            {
+                if (has.lower)
+                {
+                    Fu.name <- as.name("Su")
+                    Su.quote <- quote(eval.parent(Su))
+                }
+                else
+                {
+                    Fu.name <- as.name("Fu")
+                    Su.quote <- quote((1 - eval.parent(Fu)))
+                }
+            }
+
+            ## Calls needed in this case:
+            ## 1. one to compute F(d) if there is a deductible for the
+            ##    per loss cases;
+            ## 2. one to compute 1 - F(d) if there is a deductible for the
+            ##    per payment cases;
+            ## 3. one to compute 1 - F(u) when there is a limit.
+            ## No function to compute F(y) needed.
+            ##
+            ## If 'lower.tail' is available in 'cdf', then set it to
+            ## FALSE to compute S(d) = 1 - F(d) and S(u) = 1 - F(u)
+            ## more accurately.
+            if (deductible)             # f(y) with deductible
             {
                 Fd.name <- as.name(if (!per.loss & has.lower) "Sd" else "Fd")
                 e <- c(e,
-                       substitute(G <- Call, list(G = Fd.name)),
+                       substitute(G <- Call, list(G = Fd.name)),         # 1. or 2.
+                       if (!per.loss & has.lower) quote(Sd$lower.tail <- FALSE),
                        substitute(G[[1L]] <- as.name(fun),
                                   list(G = Fd.name, fun = as.character(Call$cdf))),
-                       substitute(G[[2L]] <- a, list(G = Fd.name, a = d)),
-                       if (!per.loss & has.lower) quote(Sd$lower.tail <- FALSE))
-                if (limit < Inf)
-                {
-                    Fu.name <- as.name(if (has.lower) "Su" else "Fu")
+                       substitute(names(G)[2L] <- q, list(G = Fd.name, q = argn[1])),
+                       substitute(G[[2L]] <- a, list(G = Fd.name, a = d)))
+                if (has.limit)
                     e <- c(e,
-                           substitute(H <- G, list(H = Fu.name, G = Fd.name)),
-                           substitute(Su[[2L]] <- a, list(a = u)),
-                           if (per.loss & has.lower) quote(Su$lower.tail <- FALSE))
-                }
+                           substitute(H <- G, list(H = Fu.name, G = Fd.name)), # 3.
+                           if (per.loss & has.lower) quote(Su$lower.tail <- FALSE),
+                           substitute(H[[2L]] <- a, list(H = Fu.name, a = u)))
             }
-            else
+            else                        # f(y) with limit only
             {
-                ## Since needs.cdf == TRUE, then this case necessarily
-                ## has limit < Inf and we need to compute F(u).
+                ## Since 'needs.cdf == TRUE', then this case
+                ## necessarily has 'limit < Inf'. Only call needed is
+                ## one to compute 1 - F(u).
                 e <- c(e,
-                       Su <- Call,
-                       substitute(Su[[1L]] <- as.name(fun),
-                                  list(fun = as.character(Call$cdf))),
-                       substitute(Su[[2L]] <- a, list(a = u)),
-                       if (has.lower) quote(Su$lower.tail <- FALSE))
+                       substitute(G <- Call, list(G = Fu.name)),
+                       if (has.lower) quote(Su$lower.tail <- FALSE),
+                       substitute(G[[1L]] <- as.name(fun),
+                                  list(G = Fu.name, fun = as.character(Call$cdf))),
+                       substitute(names(G)[2L] <- q, list(G = Fu.name, q = argn[1])),
+                       substitute(G[[2L]] <- a, list(G = Fu.name, a = u)))
             }
         }
-
-        ## Fd <- Call,
-        ## substitute(Fd[[1L]] <- fun, list(fun = as.name(Call$cdf))),
-        ## substitute(names(Fd)[[2L]] <- q, list(q = argn[1L])),
-        ## if (has.lower) Fd$lower.tail <- FALSE,
-        ## substitute(Fd[[2L]] <- x, list(x = d)),
-        ## Fu <- Fd,
-        ## Fu[[2L] <- x, list(x = u))
-
-        ## We will need to compute 1 - F(d) and 1 - F(u) in future
-        ## expressions. We definite the expressions to do these
-        ## computations here; they will differ depending if
-        ## 'lower.tail = FALSE' is available or not in 'cdf'.
-        ## if (has.lower)
-        ## {
-        ##     Fd$lower.tail <- FALSE
-        ##     Sd <- substitute(Fd(a), list(a = d))
-        ##     Su <- substitute(Fu(a), list(a = u))
-        ## }
-        ## else
-        ## {
-        ##     Sd <- substitute(1 - Fd(a), list(a = d))
-        ##     Su <- substitute(1 - Fu(a), list(a = u))
-        ## }
     }
 
     ## Repeat same steps as above for case needing the pdf. The output
@@ -167,7 +172,6 @@ coverage <- function(pdf, cdf, deductible = 0, franchise = FALSE,
         argn <- setdiff(names(argv), "log") # drop argument 'log'
         argsFUN <- argv[argn]           # arguments of output function
         x <- as.name(argn[1])           # symbol
-        print(mode(e))
         e <- c(e,
                quote(f <- Call),
                substitute(f[[1L]] <- as.name(fun),
@@ -230,27 +234,46 @@ coverage <- function(pdf, cdf, deductible = 0, franchise = FALSE,
         cond3 <- substitute(x >= b, list(x = x, b = bound2))
         f2 <- quote(eval.parent(F))
         if (!per.loss & deductible)
-            f2 <- substitute((f - g)/h,
-                             list(f = f2,
-                                  g = if (has.lower) quote(eval.parent(Fd)) else quote((q <- eval.parent(Fd))),
-                                  h = if (has.lower) quote(eval.parent(Sd)) else quote((1 - q))))
+            f2 <- if (has.lower)
+                substitute((f - F)/S,
+                           list(f = f2,
+                                F = quote(eval.parent(Fd)),
+                                S = quote(eval.parent(Sd))))
+            else
+                substitute((f - F)/S,
+                           list(f = f2,
+                                F = quote((p <- eval.parent(Fd))),
+                                S = quote((1 - p))))
         e <- c(e,
                substitute(w <- which(cond), list(cond = cond2)),
                substitute(F[[2L]] <- x, list(x = x.mod)),
                substitute(res[w] <- f, list(f = f2)),
-               substitute(res[cond] <- 1, list(cond = cond3)))
+               if (has.limit) substitute(res[cond] <- 1, list(cond = cond3)))
     }
     else
     {
         cond3 <- substitute(x == b, list(x = x, b = bound2))
-        Sd.quote <- if (has.lower) quote(eval.parent(Sd)) else quote((1 - eval.parent(Fd)))
         f2 <- quote(eval.parent(f))
-        f3 <- if (has.lower) quote(eval.parent(Su)) else quote((1 - eval.parent(Fu)))
+        if (has.limit) f3 <- Su.quote
         if (!per.loss & deductible)
         {
-            f2 <- substitute(f/S, list(f = f2, S = Sd.quote))
-            if (limit < Inf)
-                f3 <- substitute(f/S, list(f = f3, S = Sd.quote))
+            if (has.limit)
+            {
+                f2 <- if (has.lower)
+                    substitute(f/(p <- S),
+                               list(f = f2, S = quote(eval.parent(Sd))))
+                else
+                    substitute(f/(p <- S),
+                               list(f = f2, S = quote(1 - eval.parent(Fd))))
+                f3 <- substitute(f/p, list(f = f3))
+            }
+            else
+                f2 <- if (has.lower)
+                    substitute(f/S,
+                               list(f = f2, S = quote(eval.parent(Sd))))
+                else
+                    substitute(f/S,
+                               list(f = f2, S = quote((1 - eval.parent(Fd)))))
         }
         if (inflation | coinsurance < 1)
             f2 <- substitute(f/k, list(f = f2, k = coinsurance * r))
@@ -258,16 +281,11 @@ coverage <- function(pdf, cdf, deductible = 0, franchise = FALSE,
                substitute(w <- which(cond), list(cond = cond2)),
                substitute(f[[2L]] <- x, list(x = x.mod)),
                substitute(res[w] <- f, list(f = f2)),
-               substitute(res[cond] <- f, list(cond = cond3, f = f3)))
+               if (has.limit) substitute(res[cond] <- f, list(cond = cond3, f = f3)))
     }
 
-    ## Definitions for the second and third branch in the output
-    ## function.
-    ## e <- c(e,
-    ##        substitute(wx <- which(cond2), list(cond2 = cond2)),
-    ##        substitute(res[wx] <- f2, list(f2 = f2)),
-    ##        substitute(res[which(cond3)] <- f3, list(cond3 = cond3, f3 = f3)),
-    ##        quote(res))
+    ## Last expression of the output function.
+    e <- c(e, quote(res))
 
     ## Wrap up the output function.
     FUN <- function() {}
