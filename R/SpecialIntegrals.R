897668c24b243e2fd3cd2dffd3b708634d8e34d3
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2016-09-15 17:01:00 +0000

    Pointers to the C files in comments

--- R/SpecialIntegrals.R
+++ R/SpecialIntegrals.R
@@ -1,20 +1,26 @@
 ### ===== actuar: An R Package for Actuarial Science =====
 ###
-### Function to compute the incomplete beta function when b < 0, b !=
-### -1, -2, ... and a > 1+ floor(-b). Used mostly at the C level to
-### compute the limited expected value for distributions of the
-### transformed beta family.
+### The exponential integral and two integrals related to the
+### incomplete beta function and the incomplete gamma function. Used
+### only at the C level in the package. The functions therein provide
+### R interfaces just in case these could be useful.
 ###
-### See Appendix A of Klugman, Panjer & Willmot, Loss Models, Wiley.
+### The functions are *not* exported.
+###
+### See Appendix A of Klugman, Panjer and Willmot (2012), Loss Models,
+### Fourth Edition, Wiley.
 ###
 ### AUTHOR: Vincent Goulet <vincent.goulet@act.ulaval.ca>
 
+## see src/betaint.c
 betaint <- function(x, a, b)
     .External("actuar_do_dpq", "betaint", x, a, b, FALSE)
 
+## see src/expint.c
 expint <- function(x)
     .External("actuar_do_dpq", "expint", x, FALSE)
 
+## see src/gammaint.c
 gammaint <- function(x, a)
     .External("actuar_do_dpq", "gammaint", x, a, FALSE)
 
