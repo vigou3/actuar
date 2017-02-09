1f49f5dda3ca1f6272080fdde2d7e16339ede760
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2017-02-09 15:58:14 +0000

    really, just spacing

--- src/gammaint.c
+++ src/gammaint.c
@@ -36,7 +36,7 @@ double gammaint_raw(double x, double a)
 /* Here, assume that (x, a) are not NA and x > 0. */
 
      if (a > 0)
-	return gammafn(a) *  pgamma(x, a, 1.0, /*l._t.*/0, /*log_p*/0);
+	return gammafn(a) * pgamma(x, a, 1.0, /*l._t.*/0, /*log_p*/0);
 
     double k = ceil(-a);	/* note that k == 0 if a == 0 */
     double ap = a + k;		/* value a + k >= 0 */
