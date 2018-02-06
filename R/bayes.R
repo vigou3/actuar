1cb0e3274f4399f502fb11e61d3a64f5bfac371c
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2018-02-06 11:59:36 -0500

    support for the Pareto/gamma added

--- R/bayes.R
+++ R/bayes.R
@@ -14,74 +14,110 @@ bayes <- function(x, likelihood =
 {
     likelihood <- match.arg(likelihood)
 
-    if (likelihood == "bernoulli")
+    ## We need to treat separately the (Single Parameter, or
+    ## Translated) Pareto/Gamma case given the different form of the
+    ## individual mean and the "credibility factor" (which isn't one,
+    ## really).
+    if (likelihood == "pareto")
     {
-        if (missing(shape1) || missing(shape2))
-            stop("one of the Beta prior parameter \"shape1\" or \"scale2\" missing")
-        K = shape1 + shape2
-        coll = shape1/K
-        vars = (shape1 * shape2) * c(1, K)/(K^2 * (K + 1))
-    }
-    else if (likelihood == "poisson")
-    {
+        if (missing(min))
+            stop("lower bound of the likelihood missing")
         if (missing(shape) || (missing(rate) && missing(scale)))
-            stop("one of the gamma prior parameter \"shape\", \"rate\" or \"scale\" missing")
+            stop("one of the Gamma prior parameter \"shape\", \"rate\" or \"scale\" missing")
         coll = shape * scale
-        vars = c(coll * scale, coll)
-        K = 1/scale
-    }
-    else if (likelihood == "geometric")
-    {
-        if (missing(shape1) || missing(shape2))
-            stop("one of the Beta prior parameter \"shape1\" or \"scale2\" missing")
-        a <- shape1
-        b <- shape2
-        K <- a - 1
-        coll = b/K
-        vars <- b * (a + b - 1)/(K * (K - 1))
-        vars <- c(vars/K, vars)
-    }
-    else if (likelihood == "exponential")
-    {
-        if (missing(shape) || (missing(rate) && missing(scale)))
-            stop("one of the gamma prior parameter \"shape\", \"rate\" or \"scale\" missing")
-        K <- shape - 1
-        coll = 1/(K * scale)
-        vars = c(coll/scale, coll^2)/(shape - 2)
-    }
-    else if (likelihood == "normal")
-    {
-        if (missing(sd.lik))
-            stop ("standard deviation of the likelihood missing")
-        coll = mean
-        vars = c(sd, sd.lik)^2
-        K = var[2L]/vars[1L]
-    }
-    else
-        stop("unsupported likelihood")
+        vars = c(NA, NA)                # not pertinent here
 
-    ## In the pure Bayesian case we allow NULL data (makes sense
-    ## since there is no estimation to be carried), an atomic vector
-    ## (for a single contract), or a matrix or data frame (in which
-    ## case we compute the Bayesian premium for each contract).
-    ## Computation of the number of years 'n' and the individual means
-    ## differ if data is a vector or a matrix/data frame.
-    if (is.null(dim(x)))
-    {
-        n <- length(x)
-        ind.means <- if (n > 0) mean(x) else 0
+        ## Computation of individual means and credibility factors
+        ## differs depending on the type of data provided in argument.
+        if (is.null(x))                 # no data
+            cred <- ind.means <- 0
+        else if (is.vector(x, mode = "numeric")) # atomic vector
+        {
+            n <- length(x)
+            sumlog <- sum(log(x)) - n * log(min)
+            ind.means <- n/sumlog
+            cred <- sumlog/(scale + sumlog)
+        }
+        else                            # matrix or data frame
+        {
+            n <- ncol(x)
+            sumlog <- rowSums(log(x)) - n * log(min)
+            ind.means <- n/sumlog
+            cred <- sumlog/(scale + sumlog)
+        }
     }
+    ## Here are the usual linear Bayes cases.
     else
     {
-        n <- ncol(x)
-        ind.means <- rowMeans(x)
+        if (likelihood == "bernoulli")
+        {
+            if (missing(shape1) || missing(shape2))
+                stop("one of the Beta prior parameter \"shape1\" or \"scale2\" missing")
+            K = shape1 + shape2
+            coll = shape1/K
+            vars = (shape1 * shape2) * c(1, K)/(K^2 * (K + 1))
+        }
+        else if (likelihood == "poisson")
+        {
+            if (missing(shape) || (missing(rate) && missing(scale)))
+                stop("one of the Gamma prior parameter \"shape\", \"rate\" or \"scale\" missing")
+            coll = shape * scale
+            vars = c(coll * scale, coll)
+            K = 1/scale
+        }
+        else if (likelihood == "geometric")
+        {
+            if (missing(shape1) || missing(shape2))
+                stop("one of the Beta prior parameter \"shape1\" or \"scale2\" missing")
+            a <- shape1
+            b <- shape2
+            K <- a - 1
+            coll = b/K
+            vars <- b * (a + b - 1)/(K * (K - 1))
+            vars <- c(vars/K, vars)
+        }
+        else if (likelihood == "exponential")
+        {
+            if (missing(shape) || (missing(rate) && missing(scale)))
+                stop("one of the Gamma prior parameter \"shape\", \"rate\" or \"scale\" missing")
+            K <- shape - 1
+            coll = 1/(K * scale)
+            vars = c(coll/scale, coll^2)/(shape - 2)
+        }
+        else if (likelihood == "normal")
+        {
+            if (missing(sd.lik))
+                stop("standard deviation of the likelihood missing")
+            coll = mean
+            vars = c(sd, sd.lik)^2
+            K = var[2L]/vars[1L]
+        }
+        else
+            stop("unsupported likelihood")
+
+        ## Computation of individual means and credibility factors
+        ## differs depending on the type of data provided in argument.
+        if (is.null(x))                 # no data
+            cred <- ind.means <- 0
+        else if (is.vector(x, mode = "numeric")) # atomic vector
+        {
+            n <- length(x)
+            ind.means <- mean(x)
+            cred <- n/(n + K)
+        }
+        else                            # matrix or data frame
+        {
+            n <- ncol(x)
+            ind.means <- rowMeans(x)
+            cred <- n/(n + K)
+        }
     }
 
     structure(list(means = list(coll, ind.means),
                    weights = rep_len(1, n),
                    unbiased = vars,
                    iterative = NULL,
-                   cred = n/(n + K),
+                   cred = cred,
                    nodes = 1L),
               class = "bayes",
               model = "Pure Bayesian")
