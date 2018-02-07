55a89f38429f174704da9491ff2f1fe8f85425ca
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2018-02-06 22:50:01 -0500

    modified package identification in header

--- R/bstraub.R
+++ R/bstraub.R
@@ -1,4 +1,4 @@
-### ===== actuar: An R Package for Actuarial Science =====
+### actuar: Actuarial Functions and Heavy Tailed Distributions
 ###
 ### Buhlmann-Straub credibility model calculations.
 ###
--- R/cm.R
+++ R/cm.R
@@ -1,4 +1,4 @@
-### ===== actuar: An R Package for Actuarial Science =====
+### actuar: Actuarial Functions and Heavy Tailed Distributions
 ###
 ### Main interface to credibility model fitting functions.
 ###
